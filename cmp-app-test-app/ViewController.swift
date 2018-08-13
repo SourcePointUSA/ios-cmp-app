ca0f2d5558e861804d001566fbd01894305246f5
drabinowitz <dmitrirabinowitz+code@gmail.com>
2018-08-13 14:22:16 -0400

    Initial Commit

---
+++ cmp-app-test-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cmp-app-test-app
+//
+//  Created by Dmitri Rabinowitz on 8/13/18.
+//  Copyright © 2018 Sourcepoint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
