5dbdf51f0d525650d541ca30772856c729fb69ef
drabinowitz <dmitrirabinowitz+code@gmail.com>
2018-08-28 10:29:21 -0400

    Implementation with callback hook support

--- cmp-app-test-app/ConsentWebView.swift
+++ cmp-app-test-app/ConsentWebView.swift
@@ -6,16 +6,57 @@
 //  Copyright © 2018 Sourcepoint. All rights reserved.
 //
 
+public typealias Callback = (ConsentWebView) -> Void
+
 import UIKit
 import WebKit
 import JavaScriptCore
-class ConsentWebView: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+public class ConsentWebView: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
+    static public let EU_CONSENT_KEY: String = "euconsent"
+    static public let CONSENT_UUID_KEY: String = "consentUUID"
+    
+    let siteName: String
+    let page: String?
+    
+    let onReceiveMessageData: Callback?
+    let onMessageChoiceSelect: Callback?
+    let onSendConsentData: Callback?
     
     var webView: WKWebView!
+    var msgJSON: String? = nil
+    var choiceType: Int? = nil
+    var euconsent: String? = nil
+    var consentUUID: String? = nil
     var isFirstLoad = true
     
-    override func loadView() {
-        print("in load consent web view")
+    init(
+        siteName: String,
+        page: String? = nil,
+        onReceiveMessageData: Callback? = nil,
+        onMessageChoiceSelect: Callback? = nil,
+        onSendConsentData: Callback? = nil
+    ) {
+        self.siteName = siteName
+        self.page = page
+        self.onReceiveMessageData = onReceiveMessageData
+        self.onMessageChoiceSelect = onMessageChoiceSelect
+        self.onSendConsentData = onSendConsentData
+        
+        self.euconsent = UserDefaults.standard.string(forKey: ConsentWebView.EU_CONSENT_KEY)
+        self.consentUUID = UserDefaults.standard.string(forKey: ConsentWebView.CONSENT_UUID_KEY)
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    // may need to implement this eventually
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func loadView() {
+        euconsent = UserDefaults.standard.string(forKey: ConsentWebView.EU_CONSENT_KEY)
+        consentUUID = UserDefaults.standard.string(forKey: ConsentWebView.CONSENT_UUID_KEY)
         
         let config = WKWebViewConfiguration()
         let userContentController = WKUserContentController()
@@ -25,7 +66,8 @@ class ConsentWebView: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
             + "  window.webkit.messageHandlers.JSReceiver.postMessage({ name: name, body: body });\n"
             + "}\n"
             + "window.JSReceiver = {\n"
-            + "  onLoadMessage: function (willShowMessage) { postToWebView('onLoadMessage', willShowMessage); },\n"
+            + "  onReceiveMessageData: function (willShowMessage, msgJSON) { postToWebView('onReceiveMessageData', { willShowMessage: willShowMessage, msgJSON: msgJSON }); },\n"
+            + "  onMessageChoiceSelect: function (choiceType) { postToWebView('onMessageChoiceSelect', { choiceType: choiceType }); },\n"
             + "  sendConsentData: function (euconsent, consentUUID) { postToWebView('sendConsentData', { euconsent: euconsent, consentUUID: consentUUID }); }\n"
             + "};\n"
             + "})();"
@@ -44,49 +86,63 @@ class ConsentWebView: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         view = webView
     }
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         print("consent web view did load")
         webView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-        let myURL = URL(string: "http://in-app-messaging.pm.cmp.sp-stage.net/?_sp_cmp_inApp=true&_sp_writeFirstPartyCookies=true")
+        var characterSet = CharacterSet.alphanumerics
+        characterSet.insert(charactersIn: "-_.!~*'()")
+        let path = page == nil ? "" : page!
+        let siteHref = ("http://" + siteName + "/" + path).addingPercentEncoding(withAllowedCharacters: characterSet)
+        let myURL = URL(string: "http://in-app-messaging.pm.cmp.sp-stage.net/?_sp_cmp_inApp=true&_sp_writeFirstPartyCookies=true&_sp_siteHref=" + siteHref!)
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        print("view will appear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print("view did appear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print("view will disappear")
-    }
-    
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print("receiving", message)
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if let messageBody = message.body as? [String: Any], let name = messageBody["name"] as? String {
-            print("valid message body")
-            if name == "onLoadMessage" {
-                print("name is onLoadMessage")
-                if let willShowMessage = messageBody["body"] as? Bool, willShowMessage {
-                    print("will show message")
+            if name == "onReceiveMessageData" {
+                let body = messageBody["body"] as? [String: Any?]
+                
+                if let willShowMessage = body?["willShowMessage"] as? Bool, willShowMessage {
                     webView.frame = webView.superview!.frame
                 } else {
                     webView.removeFromSuperview()
                 }
-            } else if name == "sendConsentData" {
-                print("name is sendConsentData")
-                if let body = messageBody["body"] as? [String: String], let euconsent = body["euconsent"] as? String, let consentUUID = body["consentUUID"] as? String {
-                    print("euconsent: " + euconsent)
-                    print("consentUUID: " + consentUUID)
-                    let userDefaults = UserDefaults.standard
-                    userDefaults.setValue(euconsent, forKey: "euconsent")
-                    userDefaults.setValue(consentUUID, forKey: "consentUUID")
-                    userDefaults.synchronize()
+                
+                if let msgJSON = body?["msgJSON"] as? String {
+                    self.msgJSON = msgJSON
+                    self.onReceiveMessageData?(self)
                 }
+                
+            } else if name == "onMessageChoiceSelect" {
+                let body = messageBody["body"] as? [String: Int?]
+                
+                if let choiceType = body?["choiceType"] as? Int {
+                    self.choiceType = choiceType
+                    self.onMessageChoiceSelect?(self)
+                }
+                
+            } else if name == "sendConsentData" {
+                if let body = messageBody["body"] as? [String: String?], let euconsent = body["euconsent"], let consentUUID = body["consentUUID"] {
+                    let userDefaults = UserDefaults.standard
+                    if (euconsent != nil) {
+                        self.euconsent = euconsent
+                        userDefaults.setValue(euconsent, forKey: ConsentWebView.EU_CONSENT_KEY)
+                    }
+                    
+                    if (consentUUID != nil) {
+                        self.consentUUID = consentUUID
+                        userDefaults.setValue(consentUUID, forKey: ConsentWebView.CONSENT_UUID_KEY)
+                    }
+                    
+                    if (euconsent != nil || consentUUID != nil) {
+                        userDefaults.synchronize()
+                    }
+                }
+                
+                self.onSendConsentData?(self)
+                
                 webView.removeFromSuperview()
             }
         }
--- cmp-app-test-app/ViewController.swift
+++ cmp-app-test-app/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
     var consentWebView: ConsentWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("creating consent web view controller")
-        consentWebView = ConsentWebView()
-        print("adding consent web view as subview")
+        consentWebView = ConsentWebView(
+            // required, must be set second used to find scenario
+            siteName: "dev.local",
+            // optional, used for logging purposes for which page of the app the consent lib was
+            // rendered on
+            page: "main",
+            // optional, callback triggered when message data is loaded when called message data
+            // will be available as String at cbw.msgJSON
+            onReceiveMessageData: { cbw in
+                print(cbw.msgJSON as Any)
+            },
+            // optional, callback triggered when message choice is selected when called choice
+            // type will be available as Integer at cbw.choiceType
+            onMessageChoiceSelect: { cbw in
+                print(cbw.choiceType as Any)
+            },
+            // optional, callback triggered when consent data is captured when called
+            // euconsent will be available as String at cLib.euconsent and under
+            // PreferenceManager.getDefaultSharedPreferences(activity).getString(EU_CONSENT_KEY, null);
+            // consentUUID will be available as String at cLib.consentUUID and under
+            // PreferenceManager.getDefaultSharedPreferences(activity).getString(CONSENT_UUID_KEY null);
+            onSendConsentData: { cbw in
+                print(cbw.euconsent as Any, cbw.consentUUID as Any)
+            }
+        )
         view.addSubview(consentWebView.view)
     }
 }
