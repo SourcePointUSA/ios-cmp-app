9a5b78966af525a60e1cb977f67f1f1eab036c75
André Herculano <andresilveirah@gmail.com>
2019-03-14 09:52:23 +0000

    implement Consent class

---
+++ Consent.swift
@@ -0,0 +1,33 @@
+//
+//  Consent.swift
+//  ConsentViewController
+//
+//  Created by Andre Herculano on 13.03.19.
+//
+
+import Foundation
+
+public class Consent: Codable, CustomStringConvertible {
+    public let id: String
+    public let name: String
+
+    public init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+
+    enum CodingKeys : String, CodingKey {
+        case id = "_id"
+        case name
+    }
+
+    public var description: String { return "Consent(id: \(id), name: \(name)" }
+}
+
+public class VendorConsent: Consent {
+    public override var description: String { return "VendorConsent(id: \(id), name: \(name)" }
+}
+
+public class PurposeConsent: Consent {
+    public override var description: String { return "PurposeConsent(id: \(id), name: \(name)" }
+}
