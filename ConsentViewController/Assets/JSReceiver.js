8ac2de8ca243cedcf4d9f0e9cda6820ef389d2ec
André Herculano <andresilveirah@gmail.com>
2019-07-11 20:14:08 +0200

    add logs to the JSReceiver js interface

--- ConsentViewController/Assets/JSReceiver.js
+++ ConsentViewController/Assets/JSReceiver.js
@@ -1,25 +1,34 @@
 (function () {
+    console.log("[JSReceiver] Loading JSReceiver.js file");
     function postToWebView (name, body) {
+        console.log("[JSReceiver] postToWebView: "+name+body);
         window.webkit.messageHandlers.JSReceiver.postMessage({ name: name, body: body });
     }
     window.JSReceiver = {
         onReceiveMessageData: function (messageData) {
+            console.log("[JSReceiver] onReceiveMessageData: "+messageData);
             postToWebView('onReceiveMessageData', JSON.parse(messageData));
         },
         onMessageChoiceSelect: function (choiceType) {
+            console.log("[JSReceiver] onMessageChoiceSelect: "+choiceType);
             postToWebView('onMessageChoiceSelect', { choiceType: choiceType });
         },
         onErrorOccurred: function (errorType) {
+            console.log("[JSReceiver] onErrorOccurred: "+errorType);
             postToWebView('onErrorOccurred', { errorType: errorType });
         },
         sendConsentData: function (euconsent, consentUUID) {
-            postToWebView('interactionComplete', { euconsent: euconsent, consentUUID: consentUUID });
+            console.log("[JSReceiver] sendConsentData: "+"euconsent"+euconsent+"consentUUID"+consentUUID);
+            postToWebView('onInteractionComplete', { euconsent: euconsent, consentUUID: consentUUID });
         },
         onPrivacyManagerChoiceSelect: function (choiceData) {
+            console.log("[JSReceiver] onPrivacyManagerChoiceSelect: "+choiceData);
             postToWebView('onPrivacyManagerChoiceSelect', { choiceData: choiceData });
         },
         onMessageChoiceError: function (errorObj) {
+            console.log("[JSReceiver] onMessageChoiceError: "+errorObj);
             postToWebView('onMessageChoiceError', { error: errorObj.error });
         }
     };
+    console.log("[JSReceiver] Done Loading JSReceiver.js file");
 })();
