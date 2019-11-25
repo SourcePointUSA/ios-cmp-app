55c27a935f9c32db6c5d7febf673ff49d7fc923f
Andre Herculano <andresilveirah@gmail.com>
2019-11-25 14:39:26 +0100

    re-add test target to Example app

--- Example/ConsentViewController_ExampleTests/ConsentViewController_ExampleTests.swift
+++ Example/ConsentViewController_ExampleTests/ConsentViewController_ExampleTests.swift
@@ -1,27 +1,33 @@
+//
+//  ConsentViewController_ExampleTests.swift
+//  ConsentViewController_ExampleTests
+//
+//  Created by Andre Herculano on 25.11.19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
 import XCTest
 
-class Tests: XCTestCase {
-    
+class ConsentViewController_ExampleTests: XCTestCase {
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure() {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
