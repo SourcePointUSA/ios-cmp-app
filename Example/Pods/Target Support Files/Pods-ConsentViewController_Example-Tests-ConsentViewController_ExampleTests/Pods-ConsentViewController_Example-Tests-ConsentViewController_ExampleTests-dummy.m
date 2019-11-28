b6914f81cfb1f085ab7107cbe212e564b82ba332
Andre Herculano <andresilveirah@gmail.com>
2019-11-28 13:41:15 +0100

    add Quick and Nimble for testing

---
+++ Example/Pods/Target Support Files/Pods-ConsentViewController_Example-Tests-ConsentViewController_ExampleTests/Pods-ConsentViewController_Example-Tests-ConsentViewController_ExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ConsentViewController_Example_Tests_ConsentViewController_ExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_ConsentViewController_Example_Tests_ConsentViewController_ExampleTests
+@end
