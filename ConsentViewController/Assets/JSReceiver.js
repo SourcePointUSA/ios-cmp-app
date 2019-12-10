fd3e07ed76c2e89b47509e07dcc2ef31b21ddf30
Andre Herculano <andresilveirah@gmail.com>
2019-12-10 17:52:38 +0100

    update JSReceiver.js to work without ClientJS

--- ConsentViewController/Assets/JSReceiver.js
+++ ConsentViewController/Assets/JSReceiver.js
@@ -1,34 +1,79 @@
 (function () {
-    function postToWebView (name, body) {
-        window.webkit.messageHandlers.JSReceiver.postMessage({ name: name, body: body });
-    }
-    window.JSReceiver = {
-        onMessageReady: function () {
-            postToWebView('onMessageReady');
+  var postToWebView = function (webview) {
+      return function(name, body) {
+          webview.postMessage({ name: name, body: body || {} });
+      }
+  }(window.webkit.messageHandlers.JSReceiver)
+
+  window.SDK = function (postToWebView) {
+    return {
+        onMessageReady: function() {
+            postToWebView("onMessageReady");
         },
-        onMessageChoiceSelect: function (choiceId) {
-            postToWebView('onMessageChoiceSelect', { choiceId: choiceId });
+        onAction: function(action) { // action: { id: Int, type: Int }
+            postToWebView("onAction", action);
         },
-        onPrivacyManagerAction: function (pmData) {
-            postToWebView('onPrivacyManagerAction', { pmData: pmData });
+        onError: function(error) {
+            postToWebView("onError", error);
         },
-        onMessageChoiceError: function (errorObj) {
-            postToWebView('onMessageChoiceError', { error: errorObj });
-        },
-        onConsentReady: function (consentUUID, euconsent) {
-            postToWebView('onConsentReady', { consentUUID: consentUUID, euconsent: euconsent });
-        },
-        onErrorOccurred: function (errorType) {
-            postToWebView('onErrorOccurred', { errorType: errorType });
-        },
-        onPMCancel: function () {
-            postToWebView('onPMCancel');
-        },
-        onSPPMObjectReady: function () {
-            postToWebView('onSPPMObjectReady');
-        },
-        xhrLog: function (logPayload) {
-            postToWebView('xhrLog', JSON.parse(logPayload));
+        onMessageEvent: function(payload) {
+            postToWebView("onMessageEvent", payload);
         }
-    };
+    }
+  }(postToWebView)
+
+  var getActionFrom = function (payload) {
+    var actions = payload.actions || []
+    var choiceAction = actions.filter(function (action) { return action.type === 'choice' })[0] || {}
+    var choiceData = choiceAction.data || {}
+    return { id: choiceData.choice_id, type: choiceData.type };
+  }
+ 
+ var handlePMEvent = function(SDK) {
+   return function(eventName, eventPayload) {
+     var payload = eventPayload || {}
+      switch(eventName) {
+        case "sp.pmComplete":
+            SDK.onAction({ id: 99, type: 99 }); // TODO: change to real id/type when the new PM is implemented
+            break;
+        case "sp.cancel":
+            SDK.onAction({ id: 98, type: 98 }); // TODO: change to real id/type when the new PM is implemented
+            break;
+        default:
+            payload.action = eventName
+            SDK.onMessageEvent(payload)
+      }
+    }
+  }
+
+ var handleMessageEvent = function(SDK) {
+   return function(eventName, eventPayload) {
+     var payload = eventPayload || {}
+    switch(eventName) {
+        case "sp.showMessage":
+            SDK.onMessageReady();
+            break;
+        case "sp.hideMessage":
+            SDK.onAction(getActionFrom(payload));
+            break;
+        default:
+            payload.action = eventName
+            SDK.onMessageEvent(payload)
+      }
+    }
+  }
+
+  var handleMessageOrPMEvent = function (SDK) {
+    return function (event) {
+        try {
+          var payload = event.data || {};
+          var eventName = payload.name;
+          payload.action ?
+            handlePMEvent(SDK)(payload.action, payload.data) :
+            handleMessageEvent(SDK)(eventName, payload)
+        } catch (error) { SDK.onError(error); }
+    }
+  }(window.SDK)
+
+  window.addEventListener('message', handleMessageOrPMEvent);
 })();
