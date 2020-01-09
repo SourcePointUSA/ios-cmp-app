bbbcaf5fb94ffd20e9f2cd655ad61fdc6b52b4d8
Andre Herculano <andresilveirah@gmail.com>
2020-01-09 11:46:28 +0100

    Update MessageViewController.swift

--- ConsentViewController/Classes/MessageViewController.swift
+++ ConsentViewController/Classes/MessageViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 import WebKit
 
-protocol MessageUIDelegate {
+@objc public protocol MessageUIDelegate {
     func loadMessage(fromUrl url: URL)
     func loadPrivacyManager()
 }
 
-class MessageViewController: UIViewController, MessageUIDelegate {
+/// The `MessageViewController` is the class responsible for rendering the consent message and privacy manager.
+/// - Note: at the moment we only have one child of `MessageViewController` (`MessageWebViewController`) but the idea is to be able to swap the webview with any other class that knows how to render a consent message and a privacy manager. Eg. a native message view controller
+@objcMembers open class MessageViewController: UIViewController, MessageUIDelegate {
     var consentDelegate: ConsentDelegate?
-    func loadMessage(fromUrl url: URL) {}
-    func loadPrivacyManager() {}
+    public func loadMessage(fromUrl url: URL) {}
+    public func loadPrivacyManager() {}
 }
