e0089b6514c0321ef5f531f72d9a9d87b71bc891
Andre Herculano <andresilveirah@gmail.com>
2020-01-09 11:43:11 +0100

    Update ConsentDelegate.swift

--- ConsentViewController/Classes/ConsentDelegate.swift
+++ ConsentViewController/Classes/ConsentDelegate.swift
@@ -7,14 +7,39 @@
 
 import Foundation
 
+/**
+ ConsentDelegate encapsulates all SDKs lifecycle methods.
+ 
+ Have a look at [SDKs Lifecycle](https://github.com/SourcePointUSA/CCPA_iOS_SDK/wiki/SDKs-Lifecycle-methods)
+*/
 @objc public protocol ConsentDelegate {
+    /// called when there's a consent Message to be shown or before the PM is shown
     @objc func consentUIWillShow()
+    
+    /// called when the consent message is about to show
     @objc optional func messageWillShow()
+    
+    /// called when the privacy manager is about to show
     @objc optional func pmWillShow()
+    
+    /// called when the privacy manager is closed
     @objc optional func pmDidDisappear()
+    
+    /// called when the consent message is closed
     @objc optional func messageDidDisappear()
-    @objc optional func onAction(_ action: Action)
+    
+    /// called when the user takes an action in the consent ui (message/PM)
+    @objc optional func onAction(_ action: Action, consents: PMConsents?)
+    
+    /// called when the consent ui is closed
     @objc func consentUIDidDisappear()
-    @objc optional func onConsentReady(consentUUID: UUID, consents: [Consent], consentString: ConsentString?)
+    
+    /// called when we finish getting the consent profile from SourcePoint's endpoints
+    /// - Parameters:
+    ///  - consentUUID: is the uuid we give to that user internally. Notice this is not the same as authId
+    ///  - userConsent: is the data structure encapsulating the consent status, rejected vendors and purposes
+    @objc optional func onConsentReady(consentUUID: ConsentUUID, userConsent: UserConsent)
+    
+    /// the `onError` function can be called at any moment during the SDKs lifecycle
     @objc optional func onError(error: ConsentViewControllerError?)
 }
