d8d645271a2731926c49afac822ebbfc2395b86a
Andre Herculano <andresilveirah@gmail.com>
2020-01-16 21:26:27 +0100

    implement Ecodable on CampaignEnv

--- ConsentViewController/Classes/CampaignEnv.swift
+++ ConsentViewController/Classes/CampaignEnv.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 /// Tells the SDK if we should load stage or public campaigns.
-@objc public enum CampaignEnv: Int {
+@objc public enum CampaignEnv: Int, Encodable {
     case Stage = 0
     case Public = 1
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue == 0 ? "stage" : "prod")
+    }
 }
