43e2f9eb57337c30248f8f01005d3c67fb8499dc
Andre Herculano <andresilveirah@gmail.com>
2020-01-27 16:49:45 -0300

    change GDPRPropertyName regex validation to accept dashes

--- ConsentViewController/Classes/GDPRPropertyName.swift
+++ ConsentViewController/Classes/GDPRPropertyName.swift
@@ -10,10 +10,14 @@ import Foundation
 /// GDPRPropertyName is the exact name of your property as created in SourcePoint's dashboard.
 /// - Important: notice that it can only contain letters, numbers, . (dots), : (semicolons) and / (slashes). The constructor will validate upon that and throw an error otherwise.
 @objcMembers open class GDPRPropertyName: NSObject, Codable {
+    
+    /// Up and lowercase letters, dots, semicollons, numbers and dashes
+    static let validPattern = "^[a-zA-Z.:/0-9-]*$"
+    
     private static func validate(_ string: String) throws -> String {
-        let regex = try NSRegularExpression(pattern: "^[a-zA-Z.:/0-9]*$", options: [])
+        let regex = try NSRegularExpression(pattern: validPattern, options: [])
         if regex.matches(in: string, options: [], range: NSMakeRange(0, string.count)).isEmpty {
-            throw InvalidArgumentError(message: "PropertyName can only include letters, numbers, '.', ':' and '/'. \(string) passed is invalid")
+            throw InvalidArgumentError(message: "PropertyName can only include letters, numbers, '.', ':', '-' and '/'. \(string) passed is invalid")
         } else {
             return string
         }
