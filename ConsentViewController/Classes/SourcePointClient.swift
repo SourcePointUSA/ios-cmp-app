4cb0b4e31aa80001c121a7baba6ce19dded77a59
Andre Herculano <andresilveirah@gmail.com>
2020-02-05 12:07:21 -0300

    remove fake api url

--- ConsentViewController/Classes/SourcePointClient.swift
+++ ConsentViewController/Classes/SourcePointClient.swift
@@ -84,11 +84,10 @@ A Http client for SourcePoint's endpoints
  - Important: it should only be used the SDK as its public API is still in constant development and is probably going to change.
  */
 class SourcePointClient {
-    static let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net")!
-    /// - TODO: Change to real wrapper api
-    static let GET_MESSAGE_CONTENTS_URL = URL(string: "https://fake-wrapper-api.herokuapp.com/gdpr/native-message")!
-    static let GET_MESSAGE_URL_URL = URL(string: "gdpr/message-url", relativeTo: SourcePointClient.WRAPPER_API)!
-    static let CONSENT_URL = URL(string: "gdpr/consent", relativeTo: SourcePointClient.WRAPPER_API)!
+    static let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net/gdpr/")!
+    static let GET_MESSAGE_CONTENTS_URL = URL(string: "native-message", relativeTo: SourcePointClient.WRAPPER_API)!
+    static let GET_MESSAGE_URL_URL = URL(string: "message-url", relativeTo: SourcePointClient.WRAPPER_API)!
+    static let CONSENT_URL = URL(string: "consent", relativeTo: SourcePointClient.WRAPPER_API)!
     static let GET_GDPR_STATUS_URL = URL(string: "https://sourcepoint.mgr.consensu.org/consent/v2/gdpr-status")!
     
     private var client: HttpClient
