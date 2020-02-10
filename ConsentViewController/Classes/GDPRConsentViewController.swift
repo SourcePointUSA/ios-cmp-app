935306e4cc0246a7097744f7a322b21d2c2f1fc9
Leonardo Doin <leonardo-doin@Leonardos-MacBook-Pro.local>
2020-02-10 12:10:24 -0300

    remove throws in hasAuthIdChanged

--- ConsentViewController/Classes/GDPRConsentViewController.swift
+++ ConsentViewController/Classes/GDPRConsentViewController.swift
@@ -191,7 +191,7 @@ public typealias TargetingParams = [String:String]
     public func loadMessage(forAuthId authId: String?) {
         if loading == .Ready {
             loading = .Loading
-            if try! hasAuthIdChanged(newAuthId: (authId)){
+            if hasAuthIdChanged(newAuthId: (authId)){
                 resetConsentData()
                 UserDefaults.standard.setValue(authId, forKey: GDPRConsentViewController.GDPR_AUTH_ID_KEY)
             }
@@ -207,7 +207,7 @@ public typealias TargetingParams = [String:String]
         }
     }
     
-    private func hasAuthIdChanged(newAuthId: String?) throws -> Bool {
+    private func hasAuthIdChanged(newAuthId: String?) -> Bool {
         let oldAuthId  = UserDefaults.standard.string(forKey: GDPRConsentViewController.GDPR_AUTH_ID_KEY) ?? nil
         return newAuthId != oldAuthId;
     }
