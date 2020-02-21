1f4333a77c338a403ef780a713e8f52d56e624f1
Andre Herculano <andresilveirah@gmail.com>
2020-02-21 19:39:05 -0300

    refactor SourcePointRequestsResponses
    * remove rejected vendors/categories from PMConsent
    * remove GDPRStatus struct

--- ConsentViewController/Classes/SourcePointRequestsResponses.swift
+++ ConsentViewController/Classes/SourcePointRequestsResponses.swift
@@ -10,10 +10,6 @@ import Foundation
 typealias Meta = String
 public typealias GDPRUUID = String
 
-struct GdprStatus: Codable {
-    let gdprApplies: Bool
-}
-
 struct MessageRequest: Encodable {
     let uuid: GDPRUUID?
     let euconsent: String
@@ -65,11 +61,10 @@ struct ActionResponse: Decodable {
 }
 
 @objc public class PMConsent: NSObject, Codable {
-    let accepted, rejected: [String]
+    let accepted: [String]
     
-    public init(accepted: [String], rejected: [String]) {
+    public init(accepted: [String]) {
         self.accepted = accepted
-        self.rejected = rejected
     }
 }
 
