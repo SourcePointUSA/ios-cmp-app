f8d45795f7a6954aca7ecbe97256569cef68aa1a
Andre Herculano <andresilveirah@gmail.com>
2020-02-21 19:37:56 -0300

    use TCFv2 endpoints on SourcePointClient

--- ConsentViewController/Classes/SourcePointClient.swift
+++ ConsentViewController/Classes/SourcePointClient.swift
@@ -84,11 +84,10 @@ A Http client for SourcePoint's endpoints
  - Important: it should only be used the SDK as its public API is still in constant development and is probably going to change.
  */
 class SourcePointClient {
-    static let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net/gdpr/")!
+    static let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net/tcfv2/v1/gdpr/")!
     static let GET_MESSAGE_CONTENTS_URL = URL(string: "native-message", relativeTo: SourcePointClient.WRAPPER_API)!
-    static let GET_MESSAGE_URL_URL = URL(string: "message-url", relativeTo: SourcePointClient.WRAPPER_API)!
-    static let CONSENT_URL = URL(string: "consent", relativeTo: SourcePointClient.WRAPPER_API)!
-    static let GET_GDPR_STATUS_URL = URL(string: "https://sourcepoint.mgr.consensu.org/consent/v2/gdpr-status")!
+    static let GET_MESSAGE_URL_URL = URL(string: "message-url?inApp=true", relativeTo: SourcePointClient.WRAPPER_API)!
+    static let CONSENT_URL = URL(string: "consent?inApp=true", relativeTo: SourcePointClient.WRAPPER_API)!
     
     private var client: HttpClient
     private lazy var json: JSON = { return JSON() }()
