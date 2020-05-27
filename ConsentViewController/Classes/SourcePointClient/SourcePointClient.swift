97c494cf23164b4ef36b4ed20bc3d7842ca14a7e
Andre Herculano <andresilveirah@gmail.com>
2020-05-27 11:37:18 +0200

    SP-3526 use fake-wrapper-api URL

--- ConsentViewController/Classes/SourcePointClient/SourcePointClient.swift
+++ ConsentViewController/Classes/SourcePointClient/SourcePointClient.swift
@@ -25,7 +25,7 @@ A Http client for SourcePoint's endpoints
  - Important: it should only be used the SDK as its public API is still in constant development and is probably going to change.
  */
 class SourcePointClient {
-    static let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net/tcfv2/v1/gdpr/")!
+    static let WRAPPER_API = URL(string: "https://fake-wrapper-api.herokuapp.com/tcfv2/v1/gdpr/")!
     static let GET_MESSAGE_CONTENTS_URL = URL(string: "native-message?inApp=true", relativeTo: SourcePointClient.WRAPPER_API)!
     static let GET_MESSAGE_URL_URL = URL(string: "message-url?inApp=true", relativeTo: SourcePointClient.WRAPPER_API)!
     static let CONSENT_URL = URL(string: "consent?inApp=true", relativeTo: SourcePointClient.WRAPPER_API)!
