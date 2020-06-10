170f063fa5ce99bd5f2709aad397736a0b2cb1b3
Andre Herculano <andresilveirah@gmail.com>
2020-06-10 18:03:56 +0200

    [SP-3631] implement SourcePointClientMock

---
+++ Example/ConsentViewController_ExampleTests/Helpers/SourcePointClientMock.swift
@@ -0,0 +1,58 @@
+//
+//  SourcePointClientMock.swift
+//  ConsentViewController_ExampleTests
+//
+//  Created by Andre Herculano on 10.06.20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+@testable import ConsentViewController
+
+// swiftlint:disable force_try function_parameter_count
+
+class SourcePointClientMock: SourcePointProtocol {
+    var customConsentResponse: CustomConsentResponse?
+    var getMessageResponse: MessageResponse?
+    var postActionResponse: ActionResponse?
+    var error: APIParsingError?
+    var postActionCalled = false, getMessageCalled = false, customConsentCalled = false
+
+    convenience init() {
+        self.init(accountId: 0, propertyId: 0, propertyName: try! GDPRPropertyName("test"), pmId: "", campaignEnv: .Stage, targetingParams: [:], timeout: 0.0)
+    }
+
+    required init(accountId: Int,
+                  propertyId: Int,
+                  propertyName: GDPRPropertyName,
+                  pmId: String,
+                  campaignEnv: GDPRCampaignEnv,
+                  targetingParams: TargetingParams,
+                  timeout: TimeInterval) {}
+
+    func getMessage(native: Bool,
+                    consentUUID: GDPRUUID?,
+                    euconsent: String,
+                    authId: String?,
+                    meta: Meta,
+                    completionHandler: @escaping (MessageResponse?, APIParsingError?) -> Void) {
+        getMessageCalled = true
+        completionHandler(getMessageResponse, error)
+    }
+
+    func postAction(action: GDPRAction, consentUUID: GDPRUUID, meta: Meta, completionHandler: @escaping (ActionResponse?, APIParsingError?) -> Void) {
+        postActionCalled = true
+        completionHandler(postActionResponse, error)
+    }
+
+    func customConsent(toConsentUUID consentUUID: String,
+                       vendors: [String],
+                       categories: [String],
+                       legIntCategories: [String],
+                       completionHandler: @escaping (CustomConsentResponse?, APIParsingError?) -> Void) {
+        customConsentCalled = true
+        completionHandler(customConsentResponse, error)
+    }
+
+    func setRequestTimeout(_ timeout: TimeInterval) {}
+}
