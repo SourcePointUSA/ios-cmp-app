aedfbc7db75bd096d773cd476746e16af7a509fe
Andre Herculano <andresilveirah@gmail.com>
2020-06-16 19:56:12 +0200

    [SP-3631] fix encoding/decoding of GDPRCampaignEnv
    JSONDecoder and Encoder do not work for single valued elements prior to iOS 11
    Example:
      try JSONEncoder().encode(GDPRCampaignEnv.Stage) will throw an exception
           -> "Top-level GDPRCampaignEnv encoded as number JSON fragment."
    As a workaround, for iOS < 11, we encode it to an array [0] or [1]
    because that works as expected when encoding/decoding
    https://forums.swift.org/t/top-level-t-self-encoded-as-number-json-fragment/11001/3

--- ConsentViewController/Classes/GDPRCampaignEnv.swift
+++ ConsentViewController/Classes/GDPRCampaignEnv.swift
@@ -31,21 +31,45 @@ import Foundation
     }
 }
 
+/// JSONDecoder and Encoder do not work for single valued elements prior to iOS 11
+/// Example:
+///   try JSONEncoder().encode(GDPRCampaignEnv.Stage) will throw an exception
+///        -> "Top-level GDPRCampaignEnv encoded as number JSON fragment."
+/// As a workaround, for iOS < 11, we encode it to an array [0] or [1]
+/// because that works as expected when encoding/decoding
+/// https://forums.swift.org/t/top-level-t-self-encoded-as-number-json-fragment/11001/3
 extension GDPRCampaignEnv: Codable {
+    static func decodeFromSingleValue(_ decoder: Decoder) throws -> GDPRCampaignEnv? {
+        let container = try decoder.singleValueContainer()
+        return GDPRCampaignEnv(stringValue: try container.decode(String.self))
+    }
+
+    static func decodeFromArray(_ decoder: Decoder) throws -> GDPRCampaignEnv? {
+        var container = try decoder.unkeyedContainer()
+        return GDPRCampaignEnv(stringValue: try container.decode(String.self))
+    }
+
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(stringValue)
+        if #available(iOS 11, *) {
+            var container = encoder.singleValueContainer()
+            try container.encode(stringValue)
+        } else {
+            var container = encoder.unkeyedContainer()
+            try container.encode(stringValue)
+        }
     }
 
     public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
         do {
-            let env = GDPRCampaignEnv.string[try container.decode(String.self)]
-            self = env!
+            if #available(iOS 11, *) {
+                self = try GDPRCampaignEnv.decodeFromSingleValue(decoder)!
+            } else {
+                self = try GDPRCampaignEnv.decodeFromArray(decoder)!
+            }
         } catch {
             throw DecodingError.dataCorrupted(DecodingError.Context(
-                codingPath: [],
-                debugDescription: "Unknown GDPRCampaignEnv"
+               codingPath: [],
+               debugDescription: "Unknown GDPRCampaignEnv"
             ))
         }
     }
