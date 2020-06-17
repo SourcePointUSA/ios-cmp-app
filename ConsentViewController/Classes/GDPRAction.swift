eff36a01dfc6635fd5dd1111e5adeffdf2e863f9
Andre Herculano <andresilveirah@gmail.com>
2020-06-17 18:10:19 +0200

    [SP-3631] added some sugar on GDPRAction enum

--- ConsentViewController/Classes/GDPRAction.swift
+++ ConsentViewController/Classes/GDPRAction.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// User actions. Its integer representation matches with what SourcePoint's endpoints expect.
-@objc public enum GDPRActionType: Int, Codable {
+@objc public enum GDPRActionType: Int, Codable, CaseIterable, CustomStringConvertible {
     case SaveAndExit = 1
     case PMCancel = 2
     case AcceptAll = 11
