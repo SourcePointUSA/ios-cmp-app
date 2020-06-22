e494f749b8bceee0482dd3cbf1560c212d50e941
Vrushali Deshpande <vrushalid@cybage.com>
2020-06-22 20:07:54 +0530

    SP-4020: Removed android related files and their references from the framework

--- AutomationFramework/src/main/java/org/framework/config/DeviceConfig.java
+++ AutomationFramework/src/main/java/org/framework/config/DeviceConfig.java
@@ -26,19 +26,4 @@ public class DeviceConfig {
         return new IOSDeviceModel(iosDeviceModels);
     }
 
-    public static AndroidDeviceModel readAndroidDeviceConfig() throws IOException {
-        byte[] jsonData = null;
-        ObjectMapper objectMapper = new ObjectMapper();
-        jsonData = Files.readAllBytes(FileUtils.getFile(FileUtility.getFile("androidDevice.json")).toPath());
-        AndroidDeviceModel[] androidDeviceModels = objectMapper.readValue(jsonData, AndroidDeviceModel[].class);
-        return new AndroidDeviceModel(androidDeviceModels);
-    }
-
-//    public static DeviceViewportModel readDeviceViewportConfig() throws IOException {
-//        byte[] jsonData = null;
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        jsonData = Files.readAllBytes(FileUtils.getFile(FileUtility.getFile("deviceViewport.json")).toPath());
-//        DeviceViewportModel[] deviceViewportModels = objectMapper.readValue(jsonData, DeviceViewportModel[].class);
-//        return new DeviceViewportModel(deviceViewportModels);
-//    }
 }
