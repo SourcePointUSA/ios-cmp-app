5011fd9401750916306cec682c1bb44631f9405b
Vilas Dilip Mane <vilasma@cybage.com>
2020-07-17 14:41:10 +0530

    Update export_ipa.sh

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-cd /Users/runner/work/ios-cmp-app/ios-cmp-app/Example/
+cd $META_APP_HOME
 xcodebuild -archivePath "$PWD/build/SourcePointMetaApp.xcarchive" \
             -exportOptionsPlist "$PWD/SourcePointMetaApp/exportOptions.plist" \
             -exportPath "$PWD/build" \
