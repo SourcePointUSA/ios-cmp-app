4561d2b5d11214c57ed86e86677c7d9bb64736a6
vilasma <vilasma@cybage.com>
2020-08-05 00:48:47 +0530

    Add UI tests

--- Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
+++ Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ConsentViewController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,7 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if CommandLine.arguments.contains("-cleanAppsData") {
+            GDPRConsentViewController.clearAllData()
+        }
         return true
     }
 
