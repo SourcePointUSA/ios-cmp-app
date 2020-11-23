fd395dd184d79e00570932195b02d18d999f6673
Andre Herculano <andresilveirah@gmail.com>
2020-11-23 16:00:47 +0100

    remove unnused js assets

--- ConsentViewController/Assets/javascript/setAuthId.spec.js
+++
@@ -1,8 +0,0 @@
-require("./setAuthId")
-
-describe("setAuthId", () => {
-    it("should set the authId cookie", () => {
-        window.setAuthId("foo")
-        expect(document.cookie).toContain("authId=foo")
-    })
-})
