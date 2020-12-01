f1d07dceefe21634c03e0342b3c306fcb8faf631
vilasma <vilasma@cybage.com>
2020-12-02 00:00:02 +0530

    MessageLanguage enum in its own file

--- ConsentViewController/Classes/GDPRAction.swift
+++ ConsentViewController/Classes/GDPRAction.swift
@@ -62,39 +62,3 @@ import Foundation
         self.payload = payload
     }
 }
-
-/// Languages supported for Message and PM
-public enum MessageLanguage: String {
-    case English = "EN"
-    case Bulgarian = "BG"
-    case Catalan = "CA"
-    case Chinese = "ZH"
-    case Croatian = "HR"
-    case Czech = "CS"
-    case Danish = "DA"
-    case Dutch = "NL"
-    case Estonian = "ET"
-    case Finnish = "FI"
-    case French = "FR"
-    case Gaelic = "GD"
-    case German = "DE"
-    case Greek = "EL"
-    case Hungarian = "HU"
-    case Icelandic = "IS"
-    case Italian = "IT"
-    case Japanese = "JA"
-    case Latvian = "LV"
-    case Lithuanian = "LT"
-    case Norwegian = "NO"
-    case Polish = "PL"
-    case Portuguese = "PT"
-    case Romanian = "RO"
-    case Russian = "RU"
-    case Serbian_Cyrillic = "SR-CYRL"
-    case Serbian_Latin = "SR-LATN"
-    case Slovakian = "SK"
-    case Slovenian = "SL"
-    case Spanish = "ES"
-    case Swedish = "SV"
-    case Turkish = "TR"
-}
