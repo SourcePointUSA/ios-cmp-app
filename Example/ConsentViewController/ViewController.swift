73e804c81f1e7f92820809708e612abf8b156b20
André Herculano <andresilveirah@gmail.com>
2021-01-15 14:37:39 +0100

    implement an IDFA mock message

--- Example/ConsentViewController/ViewController.swift
+++ Example/ConsentViewController/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AppTrackingTransparency
+import AdSupport
 import ConsentViewController
 
 func afterFakeDelay (execute: @escaping () -> Void) {
@@ -35,6 +37,43 @@ func afterFakeDelay (execute: @escaping () -> Void) {
     func loadMessage(_ contents: [String: String])
 }
 
+@objc protocol SPConsents {
+    var uuid: UUID { get }
+    var meta: String { get }
+    var idfaStatus: SPIDFAStatus { get }
+}
+
+/// TODO: Don't forget to set the NSUserTrackingUsageDescription on the apps Info.plist otherwise we can't request access to IDFA
+@objc enum SPIDFAStatus: Int, Codable, CaseIterable, CustomStringConvertible {
+    case unknown = 0
+    case accepted = 1
+    case denied = 2
+    case unavailable = 3
+
+    public var description: String {
+        switch self {
+        case .unknown: return "unknown"
+        case .accepted: return "accepted"
+        case .denied: return "denied"
+        case .unavailable: return "unavailable"
+        }
+    }
+
+    @available(iOS 14, *)
+    init(fromApple status: ATTrackingManager.AuthorizationStatus) {
+        switch status {
+        case .authorized:
+            self = .accepted
+        case .denied, .restricted:
+            self = .denied
+        case .notDetermined:
+            self = .unknown
+        @unknown default:
+            self = .unknown
+        }
+    }
+}
+
 @objc enum SPActionType: Int, Codable, CaseIterable, CustomStringConvertible {
     case SaveAndExit = 1
     case PMCancel = 2
@@ -43,6 +82,7 @@ func afterFakeDelay (execute: @escaping () -> Void) {
     case RejectAll = 13
     case Dismiss = 15
     /// TODO: confirm with Mike we'll have a special action type for the IDFA
+    /// TODO: will the rendering app support preloading IDFA message?
     case IDFAOk = 98
     case Unknown = 99
 
@@ -54,6 +94,7 @@ func afterFakeDelay (execute: @escaping () -> Void) {
         case .SaveAndExit: return "SaveAndExit"
         case .Dismiss: return "Dismiss"
         case .PMCancel: return "PMCancel"
+        case .IDFAOk: return "IDFAOk"
         default: return "Unknown"
         }
     }
@@ -79,36 +120,71 @@ func afterFakeDelay (execute: @escaping () -> Void) {
     }
 }
 
-@objcMembers class SPGDPRConsents: NSObject {
-    override var description: String { "SPGDPRConsents()" }
+@objcMembers class SPGDPRConsents: NSObject, SPConsents {
+    let uuid: UUID
+    let meta: String
+    let idfaStatus: SPIDFAStatus
+    init(uuid: UUID, meta: String, idfaStatus: SPIDFAStatus) {
+        self.uuid = uuid
+        self.meta = meta
+        self.idfaStatus = idfaStatus
+    }
 }
-@objcMembers class SPCCPAConsents: NSObject {
-    override var description: String { "SPCCPAConsents()" }
+
+@objcMembers class SPCCPAConsents: NSObject, SPConsents {
+    let uuid: UUID
+    let meta: String
+    let idfaStatus: SPIDFAStatus
+
+    init(uuid: UUID, meta: String, idfaStatus: SPIDFAStatus) {
+        self.uuid = uuid
+        self.meta = meta
+        self.idfaStatus = idfaStatus
+    }
 }
 
 @objcMembers class SPConsentViewController: UIViewController, SPRenderingApp {
+    class ActionButton: UIButton {
+        var action: SPActionType!
+    }
     weak var consentUIDelegate: (SPConsentUIDelegate & SPRenderingAppDelegate)?
     var contents: [String: String] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+
         let title = UILabel(frame: CGRect(x: 0, y: 50, width: view.frame.width, height: 60))
         title.font = UIFont(name: "Arial", size: 60.0)
         title.text = contents["Title"]
         title.adjustsFontSizeToFitWidth = true
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-        button.center = view.center
-        button.backgroundColor = .systemBlue
-        button.setTitle(contents["AcceptAllLabel"], for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        view.backgroundColor = .white
         view.addSubview(title)
-        view.addSubview(button)
+
+        if contents["AcceptAllLabel"] != nil {
+            let button = ActionButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+            button.center = view.center
+            button.backgroundColor = .systemBlue
+            button.setTitle(contents["AcceptAllLabel"], for: .normal)
+            button.action = .AcceptAll
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            view.addSubview(button)
+        }
+
+        if contents["IDFAOkLabel"] != nil {
+            let button = ActionButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+            button.center = view.center
+            button.backgroundColor = .systemBlue
+            button.setTitle(contents["IDFAOkLabel"], for: .normal)
+            button.action = .IDFAOk
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            view.addSubview(button)
+        }
     }
 
     @objc func buttonAction(sender: UIButton!) {
-        onEvent(["action": "AcceptAll"])
+        let action = (sender as? ActionButton)?.action
+        onEvent(["action": String(action?.rawValue ?? 0)])
     }
 
     func loadMessage(_ contents: [String : String]) {
@@ -117,9 +193,8 @@ func afterFakeDelay (execute: @escaping () -> Void) {
     }
 
     func onEvent(_ payload: [String: String]) {
-        if payload["action"] == "AcceptAll" {
-            consentUIDelegate?.onAction(SPConsentAction(type: .AcceptAll))
-        }
+        let action  = SPActionType.init(rawValue: Int(payload["action"]!) ?? 0) ?? .Unknown
+        consentUIDelegate?.onAction(SPConsentAction(type: action))
     }
 }
 
@@ -182,8 +257,8 @@ func afterFakeDelay (execute: @escaping () -> Void) {
             self?.consentUI = SPConsentViewController()
             self?.consentUI.consentUIDelegate = self
             self?.consentUI.loadMessage([
-                                    "Title": "Fake Message",
-                                    "AcceptAllLabel": "Accept All"
+                                    "Title": "Fake IDFA Message",
+                                    "IDFAOkLabel": "That's Ok"
             ])
         }
         // 2b. otherwise call onConsentReady
@@ -201,15 +276,16 @@ func afterFakeDelay (execute: @escaping () -> Void) {
         }
     }
 
-    func report(action: SPConsentAction) {
+    func report(action: SPConsentAction, completionHandler: @escaping (Result<SPGDPRConsents, Error>) -> Void) {
         // send consent action
         // we will need to know what legislation that action is referring to
         // in oder to send to the right endpoint and call the appropriate consentReady delegate
-        afterFakeDelay { [weak self] in
-            // on result store
-            // 1. store consent data in the user storage
-            // 2. call onConsentReady
-            self?.delegate?.onConsentReady?(consents: SPGDPRConsents())
+        afterFakeDelay {
+            completionHandler(.success(SPGDPRConsents(
+                uuid: UUID(),
+                meta: "{}",
+                idfaStatus: .accepted
+            )))
         }
     }
 }
@@ -225,11 +301,41 @@ extension SPConsentManager: SPRenderingAppDelegate {
     }
 }
 
+/// TODO: What happens if the developers decide to call `AcceptAll` regardless of user action?
 extension SPConsentManager: SPConsentUIDelegate {
     func onAction(_ action: SPConsentAction) {
         print("onAction(\(action.type))")
         self.delegate?.onAction(action)
-        self.report(action: action)
+        switch action.type {
+        case .AcceptAll, .RejectAll, .SaveAndExit:
+            self.report(action: action) { result in
+                switch result {
+                case .success(let consents):
+                    self.delegate?.onConsentReady?(consents: consents)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        case .IDFAOk:
+            var idfaStatus: SPIDFAStatus = .unavailable
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    idfaStatus = SPIDFAStatus(fromApple: status)
+                }
+            }
+
+            self.report(action: action) { result in
+                switch result {
+                case .success(let consents):
+                    /// TODO: load consent message after IDFA action response ?????
+                    self.delegate?.onConsentReady?(consents: consents)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        default:
+            print("")
+        }
     }
 }
 
