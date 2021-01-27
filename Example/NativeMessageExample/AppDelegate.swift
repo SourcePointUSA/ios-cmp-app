504b133a027840287cf6957aae3e62c7966a6bc1
vilasma <vilasma@cybage.com>
2021-01-27 16:13:29 +0530

    Set shouldCallErrorMetrics to false while UITesting

--- Example/NativeMessageExample/AppDelegate.swift
+++ Example/NativeMessageExample/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if CommandLine.arguments.contains("-cleanAppsData") {
             GDPRConsentViewController.clearAllData()
+            GDPRConsentViewController.shouldCallErrorMetrics = false
         }
         return true
     }
--- Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
+++ Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if CommandLine.arguments.contains("-cleanAppsData") {
             GDPRConsentViewController.clearAllData()
+            GDPRConsentViewController.shouldCallErrorMetrics = false
         }
         IQKeyboardManager.shared.enable = true
         return true
