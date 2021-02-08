9ec154c46dd5ecf646293764b784847a7d537fe6
vilasma <vilasma@cybage.com>
2021-02-08 23:10:01 +0530

    Add objective-C example app

---
+++ Example/ObjC-ExampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC-ExampleApp
+//
+//  Created by Vilas on 08/02/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/ObjC-ExampleApp/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  ObjC-ExampleApp
+//
+//  Created by Vilas on 08/02/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
