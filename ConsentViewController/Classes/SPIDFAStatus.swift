a6d1b8919245ba063a3e31ac4921eab5e42a05b9
André Herculano <andresilveirah@gmail.com>
2021-02-09 12:56:34 +0100

    document IDFA enum

--- ConsentViewController/Classes/SPIDFAStatus.swift
+++ ConsentViewController/Classes/SPIDFAStatus.swift
@@ -9,7 +9,8 @@ import Foundation
 import AppTrackingTransparency
 import AdSupport
 
-/// TODO: Don't forget to set the NSUserTrackingUsageDescription on the apps Info.plist otherwise we can't request access to IDFA
+/// Maps `ATTrackingManager.requestTrackingAuthorization` into our own enum.
+/// It covers also the case when `ATTrackingManager.AuthorizationStatus` is not available.
 @objc public enum SPIDFAStatus: Int, Codable, CaseIterable, CustomStringConvertible {
     /// the user hasn't been prompted about the IDFA yet
     case unknown = 0
@@ -19,6 +20,24 @@ import AdSupport
     case denied = 2
     /// the IDFA is not available in this version of the OS
     case unavailable = 3
+
+    /// Prompts the user to allow access to the IDFA and calls the callback with the result.
+    /// If the user has already been prompted, it calls the callback with the current status.
+    /// If the OS doesn't support `ATTrackingManager.requestTrackingAuthorization` it calls
+    /// the callback with `.unavailable`.
+    /// - Important: Don't forget to set the NSUserTrackingUsageDescription on your app's Info.plist otherwise
+    ///              iOS will throw an exception when requesting access to it.
+    /// - Parameters:
+    ///   - handler: receives the IDFA status mapped into SPIDFAStatus
+    public static func requestAuthorisation(handler: @escaping (SPIDFAStatus) -> Void) {
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                handler(SPIDFAStatus(fromApple: status))
+            }
+        } else {
+            handler(.unavailable)
+        }
+    }
 
     public var description: String {
         switch self {
