4cc8ddac13502b3c8b4f7d807541f89bbe0ba8aa
André Herculano <andresilveirah@gmail.com>
2021-02-15 17:17:51 +0100

    fix OSLogger

--- ConsentViewController/Classes/OSLogger.swift
+++ ConsentViewController/Classes/OSLogger.swift
@@ -9,9 +9,9 @@ import Foundation
 import os
 
 protocol SPLogger {
-    func log(_ message: String, _ args: [String: String])
-    func debug(_ message: String, _ args: [String: String])
-    func error(_ message: String, _ args: [String: String])
+    func log(_ message: String)
+    func debug(_ message: String)
+    func error(_ message: String)
 }
 
 struct OSLogger: SPLogger {
@@ -25,16 +25,16 @@ struct OSLogger: SPLogger {
         }
     }
 
-    func log(_ message: String, _ args: [String: String] = [:]) {
-        osLog("%s", [message, args])
+    func log(_ message: String) {
+        osLog("%s", message)
     }
 
-    func debug(_ message: String, _ args: [String: String] = [:]) {
-        osLog("%s", [message, args])
+    func debug(_ message: String) {
+        osLog("%s", message)
     }
 
-    func error(_ message: String, _ args: [String: String] = [:]) {
-        osLog("%s", [message, args])
+    func error(_ message: String) {
+        osLog("%s", message)
     }
 
     private func osLog(_ message: StaticString, _ args: CVarArg) {
