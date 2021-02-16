36038992a64306e9760537bea2f1b502d3803088
vilasma <vilasma@cybage.com>
2021-02-16 16:17:13 +0530

    Update deploy workflow

--- .github/scripts/set-metaapp-home-path.sh
+++ .github/scripts/set-metaapp-home-path.sh
@@ -2,3 +2,18 @@
 
 set -euo pipefail
 echo "META_APP_HOME=/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/" >> $GITHUB_ENV
+PodSpecFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/ConsentViewController.podspec
+ProjectFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/ConsentViewController.xcodeproj/project.pbxproj
+sdk_version=`awk 'NR==3' $PodSpecFilePath`
+new_sdk_version=$(echo $sdk_version | cut -d "'" -f 2)
+echo $new_sdk_version
+meta_app_version=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
+echo $meta_app_version
+export $new_sdk_version
+buildNumber=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
+buildNumber=$(($buildNumber + 1))
+echo "BUILD_NUMBER=$new_sdk_version" >> $GITHUB_ENV
+echo $buildNumber
+export $buildNumber
+
+
