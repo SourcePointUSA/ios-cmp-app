baf9be7f7d4621a9bec69218c5d26463aa98b311
vilasma <vilasma@cybage.com>
2021-02-16 23:15:49 +0530

    Update deploy.yml

--- .github/scripts/set-metaapp-home-path.sh
+++ .github/scripts/set-metaapp-home-path.sh
@@ -5,10 +5,21 @@ echo "META_APP_HOME=/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/" >> $GIT
 PodSpecFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/ConsentViewController.podspec
 ProjectFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/ConsentViewController.xcodeproj/project.pbxproj
 sdk_version=`awk 'NR==3' $PodSpecFilePath`
-export new_sdk_version=$(echo $sdk_version | cut -d "'" -f 2)
+new_sdk_version=$(echo $sdk_version | cut -d "'" -f 2)
+echo $new_sdk_version
 meta_app_version=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
+echo $meta_app_version
 buildNumber=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
-export buildNumber=$(($buildNumber + 1))
+buildNumber=$(($buildNumber + 1))
+echo $buildNumber
+cd /Users/runner/work/ios-cmp-app/ios-cmp-app/Example/SourcePointMetaApp/
+if [ $new_sdk_version != $meta_app_version ]
+then
+    xcrun agvtool new-marketing-version $new_sdk_version
+else
+   xcrun agvtool new-version -all $buildNumber
+fi
+
 
 
 
