03766fb074de4eebfc67da4485ee592134c766e2
Vilas Dilip Mane <vilasma@cybage.com>
2021-02-16 16:28:30 +0530

    Update set-metaapp-home-path.sh

--- .github/scripts/set-metaapp-home-path.sh
+++ .github/scripts/set-metaapp-home-path.sh
@@ -6,13 +6,10 @@ PodSpecFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/ConsentViewController
 ProjectFilePath=/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/ConsentViewController.xcodeproj/project.pbxproj
 sdk_version=`awk 'NR==3' $PodSpecFilePath`
 export new_sdk_version=$(echo $sdk_version | cut -d "'" -f 2)
-echo $new_sdk_version
 meta_app_version=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
-echo $meta_app_version
 buildNumber=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjectFilePath)
 export buildNumber=$(($buildNumber + 1))
-echo "BUILD_NUMBER=$new_sdk_version" >> $GITHUB_ENV
-echo $buildNumber
+
 
 
 
