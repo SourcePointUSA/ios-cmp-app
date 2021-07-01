e00cb3e61c9ddf9d8c8f7172e786e32253bf5a4b
André Herculano <andresilveirah@gmail.com>
2021-07-01 12:07:00 +0200

    bump version to 6.1.0

--- ConsentViewController/Classes/SPConsentManager.swift
+++ ConsentViewController/Classes/SPConsentManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 @objcMembers public class SPConsentManager: NSObject, SPSDK {
-    static public let VERSION = "6.0.4"
+    static public let VERSION = "6.1.0"
 
     static public var shouldCallErrorMetrics = true
 
