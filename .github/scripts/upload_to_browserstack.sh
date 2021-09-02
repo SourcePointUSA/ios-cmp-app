efe72742ecd303d8da650705c4c2886879bbcb1b
vilasma <vilasma@cybage.com>
2021-09-02 13:56:23 +0530

    Upload Meta app ipa to browserstack

--- .github/scripts/upload_to_browserstack.sh
+++ .github/scripts/upload_to_browserstack.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X POST "https://api-cloud.browserstack.com/app-live/upload" -F "file=@/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/build/GDPR-MetaApp.ipa"
+curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X POST "https://api-cloud.browserstack.com/app-live/upload" -F "file=@/Users/runner/work/ios-cmp-app/ios-cmp-app/Example/build/Unified-MetaApp.ipa"
