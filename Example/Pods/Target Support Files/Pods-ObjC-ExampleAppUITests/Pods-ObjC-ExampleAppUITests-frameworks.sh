766eb02d98e963dbef37141a741881425e09c915
vilasma <vilasma@cybage.com>
2021-09-30 01:53:52 +0530

    Pod install to NativePMExampleAppUITests target

--- Example/Pods/Target Support Files/Pods-ObjC-ExampleAppUITests/Pods-ObjC-ExampleAppUITests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ObjC-ExampleAppUITests/Pods-ObjC-ExampleAppUITests-frameworks.sh
@@ -175,12 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble-iOS/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick-iOS/Quick.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Nimble-iOS/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Quick-iOS/Quick.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
