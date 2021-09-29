85f59710f007f39994a73dd80c8b174ab605c838
vilasma <vilasma@cybage.com>
2021-09-30 01:55:02 +0530

    Add UI test suite for native OTT privacy manager

--- Example/NativePMExampleApp/AppDelegate.swift
+++ Example/NativePMExampleApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ConsentViewController
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if CommandLine.arguments.contains("-cleanAppsData") {
+            SPConsentManager.clearAllData()
+            SPConsentManager.shouldCallErrorMetrics = false
+        }
         return true
     }
 
