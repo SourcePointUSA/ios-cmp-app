cda9c9448cf111df5f0eb490837b30c0f1c18146
Dmytro Fedko <dmytro.f@gowombat.team>
2021-12-20 11:56:29 +0200

    linter-suggested minor fixes

--- ConsentViewController/Classes/Extensions/SPString.swift
+++ ConsentViewController/Classes/Extensions/SPString.swift
@@ -23,15 +23,13 @@ extension String {
             return nil
         }
     }
-    
     var htmlToString: String { htmlToAttributedString?.string ?? "" }
-  
     func stripOutHtml() -> String? {
-        //replace &lt; with < and &gt; with >
+        // replace &lt; with < and &gt; with >
         let decoded = self.replacingOccurrences(of: "&lt;", with: "<").replacingOccurrences(of: "&gt;", with: ">")
-        //removes any CSS inline styling
+        // removes any CSS inline styling
         let noCSS = decoded.replacingOccurrences(of: "<style>[^>]+</style>", with: "", options: .regularExpression, range: nil)
-        //removes any HTML tags
+        // removes any HTML tags
         let noHTML = noCSS.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
         return noHTML
     }
