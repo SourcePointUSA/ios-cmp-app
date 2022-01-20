fe7feefe7d85c20327e7bbb296714436a21001f5
Dmytro Fedko <dmytro.f@gowombat.team>
2022-01-20 17:59:10 +0200

    [SP-8555] SPNativeImage decodes URL of customers logo and uses it as Home View Logo

--- ConsentViewController/Classes/SourcePointClient/SPNativeUI.swift
+++ ConsentViewController/Classes/SourcePointClient/SPNativeUI.swift
@@ -164,15 +164,17 @@ class SPNativeButton: SPNativeUI {
 class SPNativeImage: SPNativeUI {
     class Settings: SPNativeUISettings {
         let src: String
-
+        let url: URL?
+        
         required init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: Keys.self)
             src = try container.decode(String.self, forKey: .src)
+            url = try container.decodeIfPresent(URL.self, forKey: .url)
             try super.init(from: decoder)
         }
 
         enum Keys: CodingKey {
-            case src
+            case src, url
         }
     }
 
