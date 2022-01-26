e59e89f7d7b17b2af33ee5c4dbd470ddf1cdf94a
Dmytro Fedko <dmytro.f@gowombat.team>
2022-01-26 19:09:23 +0200

    [SP-8236] "https://" is not included in SPPropertyName if it is already there (#357)
    * [SP-8236] "https://" is not included in SPPropertyName in case it is already there
    
    * [SP-8236] added replacing of 'http://' with 'https://' for SPPropertyName
    
    * [SP-8236] 'https' is set as prefix for property if 'https' or 'http' was not included previously
    
    * [SP-8236] 3 unit tests are implemented

---
+++ Example/ConsentViewController_ExampleTests/SPPropertyNameSpec.swift
@@ -0,0 +1,36 @@
+//
+//  SPPropertyNameSpec.swift
+//  ConsentViewController_ExampleTests
+//
+//  Created by Wombat MBP 17 on 26.01.2022.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+import Quick
+import Nimble
+@testable import ConsentViewController
+
+// swiftlint:disable force_try
+class SPPropertyNameSpec: QuickSpec {
+    override func spec() {
+        it("http:// in property are not affected") {
+            let property = "http://any"
+            let spProperty = try! SPPropertyName(property)
+            expect(spProperty.rawValue).to(equal(property))
+        }
+        
+        it("https:// in property are not affected") {
+            let property = "https://any"
+            let spProperty = try! SPPropertyName(property)
+            expect(spProperty.rawValue).to(equal(property))
+        }
+
+        it("https:// prefix is added to property name if not present") {
+            let property = "any"
+            let spProperty = try! SPPropertyName(property)
+            expect(spProperty.rawValue).to(equal("https://any"))
+        }
+    }
+}
