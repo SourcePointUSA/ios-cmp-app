27149063a9609741c9d061065186cc71042fb457
Dmytro Fedko <dmytro.f@gowombat.team>
2022-03-17 11:41:48 +0100

    Revert "[SP-8865] fixes"
    This reverts commit 23b68e088aeeaedaadb37483bbc4fa6e24b0abf9.

--- ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/SPNativeScreenViewController.swift
+++ ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/SPNativeScreenViewController.swift
@@ -48,11 +48,13 @@ extension UIFont {
             .first { UIFont.familyNames.map { $0.lowercased() }.contains($0) }
             ?? UIFont.systemFont(ofSize: fontSize).familyName
         if let adjust = adjust {
-            adjust.font = UIFont(name: family, size: fontSize)
-            while (adjust.contentSize.height > adjust.frame.size.height) {
-                print(adjust.contentSize.height.description + ">" + adjust.frame.size.height.description)
-                fontSize = CGFloat(fontSize - 0.5)
-                adjust.font = UIFont(name: family, size: fontSize)
+            if (adjust.contentSize.height > adjust.frame.size.height) {
+                var fontIncrement = 1.0;
+                while (adjust.contentSize.height > adjust.frame.size.height) {
+                    fontSize = CGFloat(fontSize - fontIncrement)
+                    adjust.font = UIFont(name: family, size: fontSize)
+                    fontIncrement+=1;
+                }
             }
         }
         self.init(name: family, size: fontSize)
