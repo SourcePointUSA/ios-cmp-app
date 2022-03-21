d38eae8f7d8209b20785c300863252d0ec5aed8a
Dmytro Fedko <dmytro.f@gowombat.team>
2022-03-21 21:11:52 +0100

    [SP-8865] content canBecomeFocused depending on content size

--- ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/SPFocusableTextView.swift
+++ ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/SPFocusableTextView.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 class SPFocusableTextView: UITextView, UITextViewDelegate {
-    override var canBecomeFocused: Bool { true }
+    override var canBecomeFocused: Bool { isContentBig }
+    var isContentBig: Bool { self.contentSize.height > self.frame.size.height }
     public var canFocusCategoryTableView: Bool = true
 
     open override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         if isFocused {
             backgroundColor = .lightGray.withAlphaComponent(0.5)
             flashScrollIndicators()
-            if (self.contentSize.height > self.frame.size.height) {
+            if isContentBig {
                 canFocusCategoryTableView = false
             }
         } else {
