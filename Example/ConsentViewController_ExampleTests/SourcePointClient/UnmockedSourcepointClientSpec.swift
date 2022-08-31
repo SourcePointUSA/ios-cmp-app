82c9358bf8e13c0eb9901caed3dafdf1de5d6a3e
André Herculano <andresilveirah@gmail.com>
2022-08-31 13:44:53 +0200

    refactor UnmockedSourcepointClientSpec

--- Example/ConsentViewController_ExampleTests/SourcePointClient/UnmockedSourcepointClientSpec.swift
+++ Example/ConsentViewController_ExampleTests/SourcePointClient/UnmockedSourcepointClientSpec.swift
@@ -15,11 +15,13 @@ import Nimble
 
 class UnmockedConnectivityManagerSpec: QuickSpec {
     override func spec() {
-        let defaultTimeout: DispatchTimeInterval = .seconds(10)
         let emptyMetaData = ConsentStatusMetaData(gdpr: nil, ccpa: nil)
         var client: SourcePointClient!
 
         describe("UnmockedConnectivityManager") {
+            beforeSuite { AsyncDefaults.timeout = .seconds(20) }
+            afterSuite { AsyncDefaults.timeout = .seconds(1) }
+
             beforeEach {
                 client = SourcePointClient(
                     accountId: 22,
@@ -32,20 +34,20 @@ class UnmockedConnectivityManagerSpec: QuickSpec {
             describe("consentStatusURLWithParams") {
                 describe("with auth id") {
                     it("should add the authId query param") {
-                        let url = try? client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: "john doe")
+                        let url = client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: "john doe")
                         expect(url?.query).to(contain("authId=john%20doe"))
                     }
                 }
 
                 describe("without auth id") {
                     it("should not add the authId query param") {
-                        let url = try? client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: nil)
+                        let url = client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: nil)
                         expect(url?.query).notTo(contain("authId="))
                     }
                 }
 
                 it("should contain all query params") {
-                    let url = try? client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: nil)
+                    let url = client.consentStatusURLWithParams(propertyId: 123, metadata: emptyMetaData, authId: nil)
                     let paramsRaw = "env=\(Constants.Urls.envParam)&hasCsp=true&metadata={}&propertyId=123&withSiteActions=false"
                     expect(url?.query).to(equal(
                         paramsRaw.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
@@ -55,7 +57,7 @@ class UnmockedConnectivityManagerSpec: QuickSpec {
 
             describe("consentStatus") {
                 it("should call the endpoint and parse the response into ConsentStatusResponse") {
-                    waitUntil(timeout: defaultTimeout) { done in
+                    waitUntil { done in
                         client.consentStatus(
                             propertyId: 17801,
                             metadata: ConsentStatusMetaData(gdpr: ConsentStatusMetaData.Campaign(hasLocalData: false, applies: true, dateCreated: nil, uuid: nil), ccpa: ConsentStatusMetaData.Campaign(hasLocalData: false, applies: true, dateCreated: nil, uuid: nil)),
