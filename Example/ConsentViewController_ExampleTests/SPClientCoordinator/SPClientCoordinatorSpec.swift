f53a24e0defbff06348ab6dd6b4618715f81adc4
André Herculano <andresilveirah@gmail.com>
2023-02-06 14:33:30 +0100

    move SPClient specs to SPClientCoordinator group

---
+++ Example/ConsentViewController_ExampleTests/SPClientCoordinator/SPClientCoordinatorSpec.swift
@@ -0,0 +1,17 @@
+//
+//  SPClientCoordinatorSpec.swift
+//  ConsentViewController_ExampleTests
+//
+//  Created by Andre Herculano on 06.02.23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import ConsentViewController
+
+class CampaignSpec: QuickSpec {
+    override func spec() {
+    }
+}
