fea3c4b4cf15a314adce87bb1284fa539f5b92b7
André Herculano <andresilveirah@gmail.com>
2023-04-03 17:53:45 +0200

    update xibs

---
+++ Example/Pods/Target Support Files/ConsentViewController-tvOS10.0/ConsentViewController-tvOS10.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ConsentViewController_tvOS10_0 : NSObject
+@end
+@implementation PodsDummy_ConsentViewController_tvOS10_0
+@end
---
+++ Example/Pods/Target Support Files/ConsentViewController-tvOS11.0/ConsentViewController-tvOS11.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ConsentViewController_tvOS11_0 : NSObject
+@end
+@implementation PodsDummy_ConsentViewController_tvOS11_0
+@end
