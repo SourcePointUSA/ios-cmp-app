f70f09a527b3c411c09b23a553020ff9891ff562
André Herculano <andresilveirah@gmail.com>
2023-04-14 12:05:27 +0200

    implement preloadConsent on WKWebView extension

--- ConsentViewController/Classes/Views/iOS/SPWebViewExtensions.swift
+++ ConsentViewController/Classes/Views/iOS/SPWebViewExtensions.swift
@@ -9,53 +9,44 @@ import Foundation
 import WebKit
 
 @objc public extension WKWebView {
-    /// Reads the value of the cookie authId
-    func getAuthId(handler: @escaping (_ authId: String?, _ error: Error?) -> Void) {
-        getCookie("authId", handler)
-    }
-
-    /// Injects the cookie `authId` in the webview before loading its content.
-    /// SourcePoint's web SDK reads the `authId` cookie and set everything up in the webview context.
-    func setConsentFor(authId: String) {
-        setCookie("authId", authId)
-    }
-
-    /// Injects the cookie `authId` in the webview before loading its content.
-    /// SourcePoint's web SDK reads the `authId` cookie and set everything up in the webview context.
-    func setCookie(_ name: String, _ value: String) {
-        configuration.userContentController.addUserScript(WKUserScript(
-            source: "document.cookie='\(name)=\(value)'",
-            injectionTime: .atDocumentStart,
-            forMainFrameOnly: true
-        ))
-    }
-
-    private func getCookiesString(_ handler: @escaping (_ cookies: String?, _ error: Error?) -> Void) {
-        evaluateJavaScript("document.cookie") { result, error in
-            if error == nil, let cookies = result as? String {
-                handler(cookies, nil)
-            } else {
-                handler(nil, error)
-            }
+    /// Injects the
+    func preloadConsent(from consents: SPUserData) {
+        let readyEventName = "sp.readyForConsent"
+        let preloadEventName = "sp.loadConsent"
+        if let consentsData = try? JSONEncoder().encode(consents.webConsents),
+           let consents = String(data: consentsData, encoding: .utf8) {
+            configuration.userContentController.addUserScript(WKUserScript(
+                source: """
+            window.postMessage({
+                name: "\(preloadEventName)",
+                consent: \(consents)
+            }, "*")
+            window.addEventListener('message', (event) => {
+                if(event && event.data && event.data.name === "\(readyEventName)") {
+                    window.postMessage({
+                        name: "\(preloadEventName)",
+                        consent: \(consents)
+                    }, "*")
+                }
+            })
+            """,
+                injectionTime: .atDocumentStart,
+                forMainFrameOnly: true
+            ))
+            evaluateJavaScript("""
+            window.postMessage({
+                name: "\(preloadEventName)",
+                consent: \(consents)
+            }, "*")
+            window.addEventListener('message', (event) => {
+                if(event && event.data && event.data.name === "\(readyEventName)") {
+                    window.postMessage({
+                        name: "\(preloadEventName)",
+                        consent: \(consents)
+                    }, "*")
+                }
+            })
+        """)
         }
-    }
-
-    func getCookies(_ handler: @escaping (_ cookies: [String: String], _ error: Error?) -> Void) {
-        getCookiesString { cookies, error in
-            if let cookies = cookies, !cookies.isEmpty {
-                handler(cookies
-                    .components(separatedBy: "; ")
-                    .compactMap { $0.components(separatedBy: "=") }
-                    .reduce(into: [String: String]()) { all, pair in
-                        all[pair[0]] = pair[1]
-                    }, nil)
-            } else {
-                handler([:], error)
-            }
         }
-    }
-
-    private func getCookie(_ name: String, _ handler: @escaping (_ authId: String?, _ error: Error?) -> Void) {
-        getCookies { handler($0[name, default: ""], $1) }
-    }
 }
