82aa85629d441d373d17d5307e1b15d21402bdc9
André Herculano <andresilveirah@gmail.com>
2023-05-09 18:38:42 +0200

    add `getAuthId` and `setConsentFor` back with deprecation warning

--- ConsentViewController/Classes/Views/iOS/SPWebViewExtensions.swift
+++ ConsentViewController/Classes/Views/iOS/SPWebViewExtensions.swift
@@ -54,4 +54,57 @@ import WebKit
             """)
         }
     }
+
+    // MARK: Deprecated
+    /// Reads the value of the cookie authId
+    @available(*, deprecated, message: "This method relies on a legacy way of sharing consent with the webview and will not work on newer versions. You should use preloadConsent(from: SPUserData) instead.")
+    func getAuthId(handler: @escaping (_ authId: String?, _ error: Error?) -> Void) {
+        getCookie("authId", handler)
+    }
+
+    /// Injects the cookie `authId` in the webview before loading its content.
+    /// SourcePoint's web SDK reads the `authId` cookie and set everything up in the webview context.
+    @available(*, deprecated, message: "This method relies on a legacy way of sharing consent with the webview and will not work on newer versions. You should use preloadConsent(from: SPUserData) instead.")
+    func setConsentFor(authId: String) {
+        setCookie("authId", authId)
+    }
+
+    /// Injects the cookie `authId` in the webview before loading its content.
+    /// SourcePoint's web SDK reads the `authId` cookie and set everything up in the webview context.
+    private func setCookie(_ name: String, _ value: String) {
+        configuration.userContentController.addUserScript(WKUserScript(
+            source: "document.cookie='\(name)=\(value)'",
+            injectionTime: .atDocumentStart,
+            forMainFrameOnly: true
+        ))
+    }
+
+    private func getCookiesString(_ handler: @escaping (_ cookies: String?, _ error: Error?) -> Void) {
+        evaluateJavaScript("document.cookie") { result, error in
+            if error == nil, let cookies = result as? String {
+                handler(cookies, nil)
+            } else {
+                handler(nil, error)
+            }
+        }
+    }
+
+    private func getCookies(_ handler: @escaping (_ cookies: [String: String], _ error: Error?) -> Void) {
+        getCookiesString { cookies, error in
+            if let cookies = cookies, !cookies.isEmpty {
+                handler(cookies
+                    .components(separatedBy: "; ")
+                    .compactMap { $0.components(separatedBy: "=") }
+                    .reduce(into: [String: String]()) { all, pair in
+                        all[pair[0]] = pair[1]
+                    }, nil)
+            } else {
+                handler([:], error)
+            }
+        }
+    }
+
+    private func getCookie(_ name: String, _ handler: @escaping (_ authId: String?, _ error: Error?) -> Void) {
+        getCookies { handler($0[name, default: ""], $1) }
+    }
 }
