a7c31a2b6393269dbd4b38abd844fb6cc302c29e
Dmytro Fedko <dmytro.f@gowombat.team>
2023-05-30 15:48:45 +0200

    [DIA-2061] fix for no saving echo

--- ConsentViewController/Classes/SourcePointClient/SourcepointClientCoordinator.swift
+++ ConsentViewController/Classes/SourcePointClient/SourcepointClientCoordinator.swift
@@ -364,7 +364,9 @@ class SourcepointClientCoordinator: SPClientCoordinator {
             state.gdprMetaData?.additionsChangeDate = gdprMetaData.additionsChangeDate
             state.gdprMetaData?.legalBasisChangeDate = gdprMetaData.legalBasisChangeDate
             state.gdprMetaData?.updateSampleFields(gdprMetaData.sampleRate)
-            storage.gdprChildPmId = gdprMetaData.childPmId ?? ""
+            if campaigns.gdpr?.groupPmId != gdprMetaData.childPmId {
+                storage.gdprChildPmId = gdprMetaData.childPmId ?? ""
+            }
         }
         if let ccpaMetaData = response.ccpa {
             state.ccpa?.applies = ccpaMetaData.applies
