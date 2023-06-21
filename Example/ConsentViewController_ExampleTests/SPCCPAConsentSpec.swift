5c92e7212ee561a181f09fa786a1f218b595df1a
André Herculano <andresilveirah@gmail.com>
2023-06-21 14:36:50 +0200

    DIA-2311 add applies to the coding keys of SPCCPAConsent
    When setting/getting SPCCPAConsent to the local storage the .applies attribute was lost because it was not part of the coding keys.

---
+++ Example/ConsentViewController_ExampleTests/SPCCPAConsentSpec.swift
@@ -0,0 +1,47 @@
+//
+//  SPCCPAConsentSpec.swift
+//  ConsentViewController_ExampleTests
+//
+//  Created by Andre Herculano on 21.06.23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+@testable import ConsentViewController
+import Foundation
+import Nimble
+import Quick
+
+// swiftlint:disable force_try force_unwrapping
+
+class SPCCPAConsentsSpec: QuickSpec {
+    override func spec() {
+        describe("static empty()") {
+            it("contain empty defaults for all its fields") {
+                let consents = SPCCPAConsent.empty()
+                expect(consents.uspstring).to(equal("1---"))
+                expect(consents.rejectedVendors).to(equal([]))
+                expect(consents.rejectedCategories).to(equal([]))
+                expect(consents.uuid).to(beNil())
+                expect(consents.applies).to(beFalse())
+                expect(consents.status).to(equal(.RejectedNone))
+            }
+        }
+
+        it("is Codable") {
+            let ccpaConsents = Result { """
+                {
+                    "applies": true,
+                    "status": "rejectedNone",
+                    "rejectedVendors": [],
+                    "rejectedCategories": [],
+                    "consentStatus": {},
+                    "signedLspa": false
+                }
+                """.data(using: .utf8)
+            }
+            let consent = try ccpaConsents.decoded() as SPCCPAConsent
+            expect(consent.applies).to(beTrue())
+            expect(consent.applies).to(beTrue())
+        }
+    }
+}
