0bce01bfba1f1706303b4177b84f105bfaff15f5
André Herculano <andresilveirah@gmail.com>
2023-07-28 11:52:27 +0200

    update buildXCFramework script

--- buildXCFramework.sh
+++ buildXCFramework.sh
@@ -2,6 +2,12 @@
 
 xcodebuild archive \
     -scheme ConsentViewController-iOS \
+    -destination 'generic/platform=iOS' \
+    -archivePath './build/ConsentViewController-iOS.framework-iOS.xcarchive' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+    -scheme ConsentViewController-iOS \
     -destination 'generic/platform=iOS Simulator' \
     -archivePath './build/ConsentViewController-iOS.framework-iphonesimulator.xcarchive' \
     SKIP_INSTALL=NO \
@@ -12,8 +18,21 @@ xcodebuild archive \
     -archivePath './build/ConsentViewController-tvOS.framework-tvossimulator.xcarchive' \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
+xcodebuild archive \
+    -scheme ConsentViewController-tvOS \
+    -destination 'generic/platform=tvOS' \
+    -archivePath './build/ConsentViewController-tvOS.framework-tvOS.xcarchive' \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
+
+rm -r ./XCFramework/ConsentViewController.xcframework
+rm -r ./XCFramework/ConsentViewController.xcframework.zip
+
 xcodebuild -create-xcframework \
     -framework './build/ConsentViewController-iOS.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/ConsentViewController.framework' \
-    -framework './build/ConsentViewController-iOS.framework-iphoneos.xcarchive/Products/Library/Frameworks/ConsentViewController.framework' \
+    -framework './build/ConsentViewController-iOS.framework-iOS.xcarchive/Products/Library/Frameworks/ConsentViewController.framework' \
     -framework './build/ConsentViewController-tvOS.framework-tvossimulator.xcarchive/Products/Library/Frameworks/ConsentViewController.framework' \
+    -framework './build/ConsentViewController-tvOS.framework-tvOS.xcarchive/Products/Library/Frameworks/ConsentViewController.framework' \
     -output './XCFramework/ConsentViewController.xcframework'
+
+zip -r ./XCFramework/ConsentViewController.xcframework.zip ./XCFramework/ConsentViewController.xcframework
