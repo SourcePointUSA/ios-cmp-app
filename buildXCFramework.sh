6785299a6f95c16ee4f7b778172214d8dd2b06c4
Maik Aßhoff <maikassh@gmail.com>
2023-10-12 11:56:00 +0200

    Add script for generating the SPM binaries analogous to the other build script

--- buildXCFramework.sh
+++ buildXCFramework.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 xcodebuild archive \
+    -project _Pods.xcodeproj \
     -scheme ConsentViewController-iOS \
     -destination 'generic/platform=iOS' \
     -archivePath './build/ConsentViewController-iOS.framework-iOS.xcarchive' \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
+    -project _Pods.xcodeproj \
     -scheme ConsentViewController-iOS \
     -destination 'generic/platform=iOS Simulator' \
     -archivePath './build/ConsentViewController-iOS.framework-iphonesimulator.xcarchive' \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
 xcodebuild archive \
+    -project _Pods.xcodeproj \
     -scheme ConsentViewController-tvOS \
     -destination 'generic/platform=tvOS Simulator' \
     -archivePath './build/ConsentViewController-tvOS.framework-tvossimulator.xcarchive' \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
 xcodebuild archive \
+    -project _Pods.xcodeproj \
     -scheme ConsentViewController-tvOS \
     -destination 'generic/platform=tvOS' \
     -archivePath './build/ConsentViewController-tvOS.framework-tvOS.xcarchive' \
