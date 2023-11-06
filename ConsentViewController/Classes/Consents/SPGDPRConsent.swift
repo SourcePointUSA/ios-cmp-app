428e09cd0efee53fb0782de6b1f18a92fb73dbb3
André Herculano <andresilveirah@gmail.com>
2023-11-06 09:59:26 +0100

    move LastMessageData to its own file

--- ConsentViewController/Classes/Consents/SPGDPRConsent.swift
+++ ConsentViewController/Classes/Consents/SPGDPRConsent.swift
@@ -7,19 +7,6 @@
 
 import Foundation
 
-struct LastMessageData: Codable {
-    var id, categoryId, subCategoryId: Int
-    var partitionUUID: String
-}
-
-extension LastMessageData {
-    init(from metadata: MessageMetaData) {
-        id = Int(metadata.messageId) ?? 0
-        categoryId = metadata.categoryId.rawValue
-        subCategoryId = metadata.subCategoryId.rawValue
-        partitionUUID = metadata.messagePartitionUUID ?? ""
-    }
-}
 
 /// A dictionary in which the keys represent the Vendor Id
 public typealias SPGDPRVendorGrants = [GDPRVendorId: SPGDPRVendorGrant]
