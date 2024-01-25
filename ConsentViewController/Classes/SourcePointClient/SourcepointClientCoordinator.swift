406f763fb2d5a083c7cca4c95c5a65e4bdc01aba
André Herculano <andresilveirah@gmail.com>
2024-01-25 19:28:03 +0000

    increase localData version to populate acceptedCategories field without user action

--- ConsentViewController/Classes/SourcePointClient/SourcepointClientCoordinator.swift
+++ ConsentViewController/Classes/SourcePointClient/SourcepointClientCoordinator.swift
@@ -81,7 +81,7 @@ extension SPSampleable {
 
 class SourcepointClientCoordinator: SPClientCoordinator {
     struct State: Codable {
-        static let version = 1
+        static let version = 2
 
         struct GDPRMetaData: Codable, SPSampleable, Equatable {
             var additionsChangeDate = SPDate.now()
