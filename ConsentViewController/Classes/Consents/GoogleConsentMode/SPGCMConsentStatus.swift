10f0fc47460b85128b63cd46ef208ac6df365060
André Herculano <andresilveirah@gmail.com>
2024-02-06 19:01:42 +0100

    move GCMConsentStatus to GCMData

--- ConsentViewController/Classes/Consents/GoogleConsentMode/SPGCMConsentStatus.swift
+++ ConsentViewController/Classes/Consents/GoogleConsentMode/SPGCMConsentStatus.swift
@@ -7,33 +7,3 @@
 
 import Foundation
 
-/// Mimics Firebase's Analytics ConsentStatus enums
-@objc public enum SPGCMConsentStatus: Int, Hashable, Equatable, RawRepresentable {
-    case granted, denied
-
-    var string: String {
-        switch self {
-            case .granted: return "granted"
-            case .denied: return "denied"
-        }
-    }
-}
-
-extension SPGCMConsentStatus: Codable {
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let stringValue = try container.decode(String.self)
-        switch stringValue {
-            case "granted": self = .granted
-            case "denied": self = .denied
-
-            default:
-                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "\(stringValue) is not a known SPFirebaseConsentType"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(string)
-    }
-}
--- ConsentViewController/Classes/Consents/GoogleConsentMode/SPGCMData.swift
+++ ConsentViewController/Classes/Consents/GoogleConsentMode/SPGCMData.swift
@@ -8,6 +8,28 @@
 import Foundation
 
 @objcMembers public class SPGCMData: NSObject, Codable {
+    /// Mimics Firebase's Analytics ConsentStatus enums
+    @objc public enum Status: Int, Hashable, Equatable, RawRepresentable, Codable {
+        public typealias RawValue = String
+
+        case granted, denied
+
+        public var rawValue: String {
+            switch self {
+                case .granted: return "granted"
+                case .denied: return "denied"
+            }
+        }
+
+        public init?(rawValue: String) {
+            switch rawValue {
+                case "granted": self = .granted
+                case "denied": self = .denied
+                default: return nil
+            }
+        }
+    }
+
     enum CodingKeys: String, CodingKey {
         case adStorage = "ad_storage"
         case analyticsStorage = "analytics_storage"
@@ -15,9 +37,9 @@ import Foundation
         case adPersonalization = "ad_personalization"
     }
 
-    let adStorage, analyticsStorage, adUserData, adPersonalization: SPGCMConsentStatus?
+    let adStorage, analyticsStorage, adUserData, adPersonalization: Status?
 
-    init(adStorage: SPGCMConsentStatus? = nil, analyticsStorage: SPGCMConsentStatus? = nil, adUserData: SPGCMConsentStatus? = nil, adPersonalization: SPGCMConsentStatus? = nil) {
+    init(adStorage: Status? = nil, analyticsStorage: Status? = nil, adUserData: Status? = nil, adPersonalization: Status? = nil) {
         self.adStorage = adStorage
         self.analyticsStorage = analyticsStorage
         self.adUserData = adUserData
