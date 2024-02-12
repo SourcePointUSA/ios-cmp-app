ebecc81ccc315d2a593edf155dad3d3cb6bd0f15
André Herculano <andresilveirah@gmail.com>
2024-02-12 21:45:21 +0100

    add firebase integration demo target

---
+++ Example/SourcepointFirebaseDemo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  SourcepointFirebaseDemo
+//
+//  Created by Andre Herculano on 12.02.24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import ConsentViewController
+import FirebaseAnalytics
+
+class ViewController: UIViewController, SPDelegate {
+    @IBOutlet weak var GCMFlagLabel: UILabel!
+    @IBOutlet weak var TCStringTextView: UITextView!
+
+    @IBAction func loadPMButton(_ sender: Any) {
+        consentManager.loadGDPRPrivacyManager(withId: "488393")
+    }
+
+    lazy var consentManager: SPConsentManager = { SPConsentManager(
+        accountId: 22,
+        propertyId: 16893,
+        propertyName: try! SPPropertyName("mobile.multicampaign.demo"),
+        campaigns: SPCampaigns(gdpr: SPCampaign()),
+        delegate: self
+    )}()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        consentManager.loadMessage(forAuthId: nil, pubData: nil)
+    }
+
+    func onAction(_ action: SPAction, from controller: UIViewController) {
+        print(action)
+    }
+
+    func onSPUIReady(_ controller: UIViewController) {
+        present(controller, animated: true)
+    }
+
+    func onSPUIFinished(_ controller: UIViewController) {
+        dismiss(animated: true)
+    }
+}
+
+// MARK: Firebase Google Consent Mode integration
+extension ViewController {
+    func onSPFinished(userData: SPUserData) {
+        GCMFlagLabel.text = "\(UserDefaults.standard.bool(forKey: "IABTCF_EnableAdvertiserConsentMode"))"
+        TCStringTextView.text = UserDefaults.standard.string(forKey: "IABTCF_TCString")
+
+        let gcmData = userData.gdpr?.consents?.googleConsentMode
+        Analytics.setConsent([
+            .analyticsStorage: gcmData?.analyticsStorage == .granted ? .granted : .denied
+        ])
+    }
+}
+
