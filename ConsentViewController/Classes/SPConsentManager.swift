4b1f74df108df195b1ea8c259fa9aeb7979e8f0f
André Herculano <andresilveirah@gmail.com>
2024-08-27 11:59:05 +0200

    'update version to 7.7.1'

--- ConsentViewController/Classes/SPConsentManager.swift
+++ ConsentViewController/Classes/SPConsentManager.swift
@@ -263,7 +263,7 @@ import UIKit
 }
 
 @objc extension SPConsentManager: SPSDK {
-    public static let VERSION = "7.7.0"
+    public static let VERSION = "7.7.1"
 
     public var gdprApplies: Bool { spCoordinator.userData.gdpr?.applies ?? false }
 
