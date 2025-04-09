bb74333c02b030a7d8d280d8d50dcf7c2f93d904
André Herculano <andresilveirah@gmail.com>
2025-04-09 13:23:59 +0200

    Update SPMessageLanguage.swift

--- ConsentViewController/Classes/SPMessageLanguage.swift
+++ ConsentViewController/Classes/SPMessageLanguage.swift
@@ -9,28 +9,29 @@ import Foundation
 
 /// Languages supported by Message and PM
 @objc public enum SPMessageLanguage: Int, Codable {
+    case Albanian
     case Arabic
     case Basque
-    case Bosnian
-    case BrowserDefault
-    case English
+    case Bosnian_Latin
     case Bulgarian
     case Catalan
-    case Chinese
+    case Chinese_Simplified
+    case Chinese_Traditional
     case Croatian
     case Czech
     case Danish
     case Dutch
+    case English
     case Estonian
     case Finnish
     case French
-    case Gaelic
     case Galician
+    case Georgian
     case German
     case Greek
     case Hebrew
+    case Hindi
     case Hungarian
-    case Icelandic
     case Indonesian
     case Italian
     case Japanese
@@ -42,207 +43,139 @@ import Foundation
     case Maltese
     case Norwegian
     case Polish
-    case Portuguese
+    case Portuguese_Brazil
+    case Portuguese_Portugal
     case Romanian
     case Russian
     case Serbian_Cyrillic
     case Serbian_Latin
-    case Slovakian
+    case Slovak
     case Slovenian
     case Spanish
+    case Swahili
     case Swedish
     case Tagalog
+    case Thai
     case Turkish
+    case Ukrainian
+    case Vietnamese
+    case Welsh
+    case BrowserDefault
 
     public typealias RawValue = String
 
     public var rawValue: RawValue {
         switch self {
-        case .Arabic:
-            return "AR"
-        // having it empty string instructs the rendering app to use the browser's locale
-        case .BrowserDefault:
-            return ""
-        case .English:
-            return "EN"
-        case .Basque:
-            return "EU"
-        case .Bosnian:
-            return "BS"
-        case .Bulgarian:
-            return "BG"
-        case .Catalan:
-            return "CA"
-        case .Chinese:
-            return "ZH"
-        case .Croatian:
-            return "HR"
-        case .Czech:
-            return "CS"
-        case .Danish:
-            return "DA"
-        case .Dutch:
-            return "NL"
-        case .Estonian:
-            return "ET"
-        case .Finnish:
-            return "FI"
-        case .French:
-            return "FR"
-        case .Gaelic:
-            return "GD"
-        case .Galician:
-            return "GL"
-        case .German:
-            return "DE"
-        case .Greek:
-            return "EL"
-        case .Hebrew:
-            return "HE"
-        case .Hungarian:
-            return "HU"
-        case .Icelandic:
-            return "IS"
-        case .Indonesian:
-            return "ID"
-        case .Italian:
-            return "IT"
-        case .Japanese:
-            return "JA"
-        case .Korean:
-            return "KO"
-        case .Latvian:
-            return "LV"
-        case .Lithuanian:
-            return "LT"
-        case .Macedonian:
-            return "MK"
-        case .Malay:
-            return "MS"
-        case .Maltese:
-            return "MT"
-        case .Norwegian:
-            return "NO"
-        case .Polish:
-            return "PL"
-        case .Portuguese:
-            return "PT"
-        case .Romanian:
-            return "RO"
-        case .Russian:
-            return "RU"
-        case .Serbian_Cyrillic:
-            return "SR-CYRL"
-        case .Serbian_Latin:
-            return "SR-LATN"
-        case .Slovakian:
-            return "SK"
-        case .Slovenian:
-            return "SL"
-        case .Spanish:
-            return "ES"
-        case .Swedish:
-            return "SV"
-        case .Tagalog:
-            return "TL"
-        case .Turkish:
-            return "TR"
+        case .Albanian: return "sq"
+        case .Arabic: return "ar"
+        case .Basque: return "eu"
+        case .Bosnian_Latin: return "bs"
+        case .Bulgarian: return "bg"
+        case .Catalan: return "ca"
+        case .Chinese_Simplified: return "zh"
+        case .Chinese_Traditional: return "zh-hant"
+        case .Croatian: return "hr"
+        case .Czech: return "cs"
+        case .Danish: return "da"
+        case .Dutch: return "nl"
+        case .English: return "en"
+        case .Estonian: return "et"
+        case .Finnish: return "fi"
+        case .French: return "fr"
+        case .Galician: return "gl"
+        case .Georgian: return "ka"
+        case .German: return "de"
+        case .Greek: return "el"
+        case .Hebrew: return "he"
+        case .Hindi: return "hi"
+        case .Hungarian: return "hu"
+        case .Indonesian: return "id"
+        case .Italian: return "it"
+        case .Japanese: return "ja"
+        case .Korean: return "ko"
+        case .Latvian: return "lv"
+        case .Lithuanian: return "lt"
+        case .Macedonian: return "mk"
+        case .Malay: return "ms"
+        case .Maltese: return "mt"
+        case .Norwegian: return "no"
+        case .Polish: return "pl"
+        case .Portuguese_Brazil: return "pt-br"
+        case .Portuguese_Portugal: return "pt-pt"
+        case .Romanian: return "ro"
+        case .Russian: return "ru"
+        case .Serbian_Cyrillic: return "sr-cyrl"
+        case .Serbian_Latin: return "sr-latn"
+        case .Slovak: return "sk"
+        case .Slovenian: return "sl"
+        case .Spanish: return "es"
+        case .Swahili: return "sw"
+        case .Swedish: return "sv"
+        case .Tagalog: return "tl"
+        case .Thai: return "th"
+        case .Turkish: return "tr"
+        case .Ukrainian: return "uk"
+        case .Vietnamese: return "vi"
+        case .Welsh: return "cy"
+        case .BrowserDefault: return ""
         }
     }
-    // swiftlint:disable:next cyclomatic_complexity function_body_length
-    public init?(rawValue: RawValue) {
-        switch rawValue.uppercased() {
-        case "":
-            self = .BrowserDefault
-        case "EN":
-            self = .English
-        case "AR":
-            self = .Arabic
-        case "EU":
-            self = .Basque
-        case "BS":
-            self = .Bosnian
-        case "BG":
-            self = .Bulgarian
-        case "CA":
-            self = .Catalan
-        case "ZH":
-            self = .Chinese
-        case "HR":
-            self = .Croatian
-        case "CS":
-            self = .Czech
-        case "DA":
-            self = .Danish
-        case "NL":
-            self = .Dutch
-        case "ET":
-            self = .Estonian
-        case "FI":
-            self = .Finnish
-        case "FR":
-            self = .French
-        case "GD":
-            self = .Gaelic
-        case "GL":
-            self = .Galician
-        case "DE":
-            self = .German
-        case "EL":
-            self = .Greek
-        case "HE":
-            self = .Hebrew
-        case "HU":
-            self = .Hungarian
-        case "IS":
-            self = .Icelandic
-        case "ID":
-            self = .Indonesian
-        case "IT":
-            self = .Italian
-        case "JA":
-            self = .Japanese
-        case "KO":
-            self = .Korean
-        case "LV":
-            self = .Latvian
-        case "LT":
-            self = .Lithuanian
-        case "MK":
-            self = .Macedonian
-        case "MS":
-            self = .Malay
-        case "MT":
-            self = .Maltese
-        case "NO":
-            self = .Norwegian
-        case "PL":
-            self = .Polish
-        case "PT":
-            self = .Portuguese
-        case "RO":
-            self = .Romanian
-        case "RU":
-            self = .Russian
-        case "SR-CYRL":
-            self = .Serbian_Cyrillic
-        case "SR-LATN":
-            self = .Serbian_Latin
-        case "SK":
-            self = .Slovakian
-        case "SL":
-            self = .Slovenian
-        case "ES":
-            self = .Spanish
-        case "SV":
-            self = .Swedish
-        case "TL":
-            self = .Tagalog
-        case "TR":
-            self = .Turkish
 
-        default:
-            return nil
+    public init?(rawValue: RawValue) {
+        switch rawValue.lowercased() {
+        case "sq": self = .Albanian
+        case "ar": self = .Arabic
+        case "eu": self = .Basque
+        case "bs": self = .Bosnian_Latin
+        case "bg": self = .Bulgarian
+        case "ca": self = .Catalan
+        case "zh": self = .Chinese_Simplified
+        case "zh-hant": self = .Chinese_Traditional
+        case "hr": self = .Croatian
+        case "cs": self = .Czech
+        case "da": self = .Danish
+        case "nl": self = .Dutch
+        case "en": self = .English
+        case "et": self = .Estonian
+        case "fi": self = .Finnish
+        case "fr": self = .French
+        case "gl": self = .Galician
+        case "ka": self = .Georgian
+        case "de": self = .German
+        case "el": self = .Greek
+        case "he": self = .Hebrew
+        case "hi": self = .Hindi
+        case "hu": self = .Hungarian
+        case "id": self = .Indonesian
+        case "it": self = .Italian
+        case "ja": self = .Japanese
+        case "ko": self = .Korean
+        case "lv": self = .Latvian
+        case "lt": self = .Lithuanian
+        case "mk": self = .Macedonian
+        case "ms": self = .Malay
+        case "mt": self = .Maltese
+        case "no": self = .Norwegian
+        case "pl": self = .Polish
+        case "pt-br": self = .Portuguese_Brazil
+        case "pt-pt": self = .Portuguese_Portugal
+        case "ro": self = .Romanian
+        case "ru": self = .Russian
+        case "sr-cyrl": self = .Serbian_Cyrillic
+        case "sr-latn": self = .Serbian_Latin
+        case "sk": self = .Slovak
+        case "sl": self = .Slovenian
+        case "es": self = .Spanish
+        case "sw": self = .Swahili
+        case "sv": self = .Swedish
+        case "tl": self = .Tagalog
+        case "th": self = .Thai
+        case "tr": self = .Turkish
+        case "uk": self = .Ukrainian
+        case "vi": self = .Vietnamese
+        case "cy": self = .Welsh
+        case "": self = .BrowserDefault
+        default: return nil
         }
     }
 }
