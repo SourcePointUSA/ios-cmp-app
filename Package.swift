4090988327183299e43795f6a16caf5871bc70ab
André Herculano <andresilveirah@gmail.com>
2025-04-14 19:29:02 +0200

    attempt pure Package.swift

--- Package.swift
+++ Package.swift
@@ -2,21 +2,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConsentViewController",
-    platforms: [
-        .iOS(.v10),
-        .tvOS(.v10)
-    ],
-    products: [
-        .library(
-            name: "ConsentViewController",
-            targets: ["ConsentViewController"]
-        ),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "ConsentViewController",
-            path: "./XCFramework/ConsentViewController.xcframework"
-        )
-    ]
+  name: "ConsentViewController",
+  platforms: [
+    .iOS(.v10),
+    .tvOS(.v10)
+  ],
+  products: [
+    .library(name: "ConsentViewController", targets: ["ConsentViewController"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "ConsentViewController",
+      dependencies: [],
+      path: "ConsentViewController",
+      exclude: [
+        "Assets/javascript/SPJSReceiver.spec.js",
+        "Assets/javascript/jest.config.json",
+      ],
+      resources: [
+        .process("Assets/javascript/SPJSReceiver.js"),
+        .process("Assets/images")
+      ]
+    )
+  ],
 )
+
+
