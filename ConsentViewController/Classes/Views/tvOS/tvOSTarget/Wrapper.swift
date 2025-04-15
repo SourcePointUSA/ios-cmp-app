e0893c011f4f97a40c4f0919e9ea7016be7a72a3
André Herculano <andresilveirah@gmail.com>
2025-04-15 18:21:28 +0200

    add tvOSWrapper

--- ConsentViewController/Classes/Views/tvOS/tvOSTarget/Wrapper.swift
+++ ConsentViewController/Classes/Views/tvOS/tvOSTarget/Wrapper.swift
@@ -1,5 +1,5 @@
 //
-//  Stub.swift
+//  Wrapper.swift
 //  Pods
 //
 //  Created by Andre Herculano on 14/4/25.
@@ -7,3 +7,5 @@
 
 // This file exists to satisfy SwiftPM's requirement for a source file.
 // The actual purpose of this target is to bundle tvOS-only resources (e.g., XIBs).
+
+@_exported import ConsentViewController
