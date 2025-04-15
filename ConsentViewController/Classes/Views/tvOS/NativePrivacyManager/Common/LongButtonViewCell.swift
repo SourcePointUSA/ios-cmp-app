864c74aee52a14320e8b8a67874b09bc27206100
André Herculano <andresilveirah@gmail.com>
2025-04-15 20:04:19 +0200

    make LongButtonViewCell public

--- ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/LongButtonViewCell.swift
+++ ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/Common/LongButtonViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LongButtonViewCell: UITableViewCell {
+public class LongButtonViewCell: UITableViewCell {
 
     public static var nib = UINib(nibName: "LongButtonViewCell", bundle: Bundle.framework)
 
@@ -52,14 +52,14 @@ class LongButtonViewCell: UITableViewCell {
         accessibilityIdentifier = "\(labelText ?? "") \(stateLabel.text ?? "")"
     }
 
-    override func prepareForReuse() {
+    public override func prepareForReuse() {
         labelText = nil
         isOn = nil
         selectable = false
         loadUI()
     }
 
-    override func awakeFromNib() {
+    public override func awakeFromNib() {
         super.awakeFromNib()
         loadUI()
         selectionStyle = .none
