3939f9bdc59bed657a5895e5b8f7fe1a419c9221
Dmytro <dmytro.f@gowombat.team>
2025-05-19 14:21:16 +0200

    Fix

--- ConsentViewController/Classes/Consents/SPPreferencesConsent.swift
+++ ConsentViewController/Classes/Consents/SPPreferencesConsent.swift
@@ -55,7 +55,7 @@ extension SPPreferencesConsent {
             messageId: messageId,
             uuid: uuid,
             status: status,
-            rejectedStatus: rejectedStatus,
+            rejectedStatus: rejectedStatus
         )
     }
 }
