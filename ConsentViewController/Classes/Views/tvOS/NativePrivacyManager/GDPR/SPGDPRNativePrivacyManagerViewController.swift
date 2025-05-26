e15600068fbf96ca294a801fc15480e042b89db7
Dmytro <dmytro.f@gowombat.team>
2025-05-26 17:55:20 +0200

    Add `unavailable`

--- ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/GDPR/SPGDPRNativePrivacyManagerViewController.swift
+++ ConsentViewController/Classes/Views/tvOS/NativePrivacyManager/GDPR/SPGDPRNativePrivacyManagerViewController.swift
@@ -331,7 +331,8 @@ class CategoryCellView: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupBackground()
     }
-    
+
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
