6078311bb59d7e6dd4b56dc8a02b7feab4cfb38d
Andre Herculano <228650+andresilveirah@users.noreply.github.com>
2025-10-28 16:14:42 +0000

    'release 7.12.7-beta.5'

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         .binaryTarget(
             name: "ConsentViewController",
             // path: "./build/SPMConsentViewController.xcframework" <-- use for local development
-            url: "https://github.com/SourcePointUSA/ios-cmp-app/releases/download/x.y.z/SPMConsentViewController.xcframework.zip",
-            checksum: "checksum here"
+            url: "https://github.com/SourcePointUSA/ios-cmp-app/releases/download/7.12.7-beta.5/SPMConsentViewController.xcframework.zip",
+            checksum: "6459aebeb8986947bfa3fd3b6099856bb15290789170f7c4489cb9224b3f4ee5"
         )
     ]
 )
